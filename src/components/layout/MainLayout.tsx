1b2bdc1e4a6b42e2f0d894dd527b8ad1864b6049
maddi02 <martin.hummel02@icloud.com>
2024-02-27 12:02:17 +0100

    Refactor logo display logic in MainLayout and Header
    The logo display management was moved from being a prop in the MainLayout to a locally managed condition in the Header component, using the route path. This makes the implementation more localized and direct, reducing unnecessary prop propagation.

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -5,15 +5,13 @@ import Footer from '@/components/ui/Footer';
 import Sidebar from '../ui/Sidebar';
 
 const MainLayout: React.FC<PropsWithChildren> = () => {
-        const location = useLocation()
-        const showLogo = location.pathname === "/"
 
         return (
             <>
                 <Sidebar/>
                 <div className="bg-[#3B3B3B]">
                     <div className="flex min-h-[100vh] flex-col">
-                        <Header showLogo={showLogo}/>
+                        <Header/>
                         <div className="flex-1">
                             <Outlet/>
                         </div>
--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 import DesktopLogo from '@/assets/logos/edulution-logo-long-colorfull.svg';
 import MobileLogo from '@/assets/logos/edulution-logo-long-colorfull-white.svg';
-import { useMediaQuery } from 'usehooks-ts';
-interface HeaderProps{
-  showLogo: boolean
-}
-const Header: React.FC<HeaderProps> = ({showLogo}) => {
-  const isDesktop = useMediaQuery('(min-width: 768px)');
-  return (
-    <div className="mb-3 pb-1">
-      <div className="flex max-w-[1440px] justify-between align-middle">
-        {showLogo ? (
-            <div className={`w-[150px] rounded-b-[8px] ${isDesktop ? 'mt-0 w-[250px] bg-white' : 'mt-4'}`}>
-              <Link to="/">
-                <img
-                    src={isDesktop ? DesktopLogo : MobileLogo}
-                    alt="edulution"
-                />
-              </Link>
+import {useMediaQuery} from 'usehooks-ts';
+
+const Header: React.FC = () => {
+    const location = useLocation()
+    const showLogo = location.pathname === "/"
+
+    const isDesktop = useMediaQuery('(min-width: 768px)');
+    return (
+        <div className="mb-3 pb-1">
+            <div className="flex max-w-[1440px] justify-between align-middle">
+                {showLogo ? (
+                    <div className={`w-[150px] rounded-b-[8px] ${isDesktop ? 'mt-0 w-[250px] bg-white' : 'mt-4'}`}>
+                        <Link to="/">
+                            <img
+                                src={isDesktop ? DesktopLogo : MobileLogo}
+                                alt="edulution"
+                            />
+                        </Link>
+                    </div>
+                ) : null}
+                {/* Add your navigation links or other content here */}
             </div>
-        ): null}
-        {/* Add your navigation links or other content here */}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Header;
