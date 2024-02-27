9420cb65e5e116b22f8469031fbe6ffa5e1253ea
maddi02 <martin.hummel02@icloud.com>
2024-02-27 10:28:55 +0100

    Update MainLayout's component structure
    MainLayout.tsx has been updated to support optional logo displays and receive children to display. The Footer component has been commented out for now and showLogo property has been added to the layout configuration. The PropsWithChildren import has been removed as it's no longer needed.

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,18 +1,26 @@
-import React, { PropsWithChildren } from "react";
-import { Outlet } from "react-router-dom";
+import React from "react";
 import { Header } from "@/components/ui/Header";
-import { Footer } from "@/components/ui/Footer";
+// import { Footer } from "@/components/ui/Footer";
 import { Sidebar } from "../ui/Sidebar";
+interface MainLayoutProps{
+    children: React.ReactNode
+    showLogo: boolean
+}
 
-export const MainLayout: React.FC<PropsWithChildren> = () => {
+export const MainLayout: React.FC<MainLayoutProps> = ({children,showLogo}) => {
   return (
     <>
       <Sidebar />
       <div className="bg-[#3B3B3B]">
-        <div className="flex min-h-[100vh] flex-col px-5 lg:px-20">
-          <Header />
-          <div className="flex-1">{<Outlet />}</div>
-          <Footer />
+        <div className="flex min-h-[100vh] flex-col">
+            {showLogo ? (
+                <div className="lg:px-20 px-5">
+                  <Header />
+                </div>
+            ):(<></>)}
+
+          <div className="flex-1">{children}</div>
+          {/*<Footer />*/}
         </div>
       </div>
     </>
