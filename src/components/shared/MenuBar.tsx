9818a64fa68978bee6e5f90d2e8c068e66d43fb0
Michael Dold <michael.dold@outlook.de>
2024-02-28 12:29:48 +0100

    Styled menu bar according to style guide

--- src/components/shared/MenuBar.tsx
+++ src/components/shared/MenuBar.tsx
@@ -1,48 +1,53 @@
 import React from 'react';
 
 import { MenubarMenu, MenubarSeparator, MenubarTrigger, VerticalMenubar } from '@/components/ui/menubar';
-import MenuItem from '../../../datatypes/types';
+import MenuItem from '@/datatypes/types';
 
-interface BasicPageLayoutProps {
+interface MenuBarProps {
   menuItems: MenuItem[];
   title: string;
-  logoImagePath: string;
+  icon: string;
+  color: string;
 }
 
-const MenuBar: React.FC<BasicPageLayoutProps> = ({ menuItems, title, logoImagePath }) => (
-  <div className="flex h-screen  overflow-hidden">
-    <VerticalMenubar
-      className="w-1/12"
-      style={{ width: '10px' }}
-    >
-      <div className="bg-left_sideBar_background h-full overflow-y-auto text-white">
-        <div className="flex w-full flex-col items-center justify-center pt-4">
-          <img
-            src={logoImagePath}
-            alt=""
-            className="h-16 w-16 object-contain "
-          />
-          <span className="mt-4 text-xl font-bold">{title}</span>
+const MenuBar: React.FC<MenuBarProps> = ({ menuItems, title, icon, color }) => {
+  const hoverColor = `hover:${color}`;
+
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <VerticalMenubar className=" bg-black bg-opacity-10 ">
+        <div className="text-white">
+          <div className="flex flex-col items-center justify-center py-5 pt-4">
+            <img
+              src={icon}
+              alt=""
+              className="h-20 w-20 object-contain"
+            />
+            <div className="mt-4 text-lg font-bold">{title}</div>
+          </div>
           <MenubarSeparator />
+          <MenubarMenu>
+            {menuItems.map((item) => (
+              <React.Fragment key={item.label}>
+                <MenubarTrigger
+                  className={`${hoverColor} flex w-full cursor-pointer items-center gap-5 px-10 py-1`}
+                  onClick={item.action}
+                >
+                  <img
+                    src={item.icon}
+                    alt=""
+                    className="h-12 w-12 object-contain "
+                  />
+                  <div className="text-base">{item.label}</div>
+                </MenubarTrigger>
+                <MenubarSeparator />
+              </React.Fragment>
+            ))}
+          </MenubarMenu>
         </div>
-        <MenubarMenu>
-          <MenubarSeparator />
-          {menuItems.map((item) => (
-            <React.Fragment key={item.label}>
-              <MenubarTrigger
-                className="flex w-full cursor-pointer items-center px-4 py-4 hover:bg-blue-800"
-                onClick={item.action}
-              >
-                <p className="mr-3 text-lg text-white" />
-                <span className="font-medium">{item.label}</span>
-              </MenubarTrigger>
-              <MenubarSeparator />
-            </React.Fragment>
-          ))}
-        </MenubarMenu>
-      </div>
-    </VerticalMenubar>
-  </div>
-);
+      </VerticalMenubar>
+    </div>
+  );
+};
 
 export default MenuBar;
--- src/pages/FileSharing/FileSharing.tsx
+++ src/pages/FileSharing/FileSharing.tsx
@@ -1,37 +1,44 @@
 import React from 'react';
-import { FiHome, FiLogOut, FiSettings, FiUser } from 'react-icons/fi';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+
+import { FileSharing as FileSharingIcon, Desktop, Share, Students } from '@/assets/icons';
 import MenuBar from '@/components/shared/MenuBar';
-import MenuItem from '../../../datatypes/types';
+import MenuItem from '@/datatypes/types';
 
 const FileSharing = () => {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
   const demoMenuItems: MenuItem[] = [
     {
-      label: 'Home',
-      action: () => {},
-      IconComponent: FiHome,
+      label: t('home'),
+      action: () => navigate('/file-sharing'),
+      icon: FileSharingIcon,
     },
     {
-      label: 'Profile',
-      action: () => {},
-      IconComponent: FiUser,
+      label: t('programs'),
+      action: () => () => navigate('/file-sharing/profile'),
+      icon: Desktop,
     },
     {
-      label: 'Settings',
-      action: () => {},
-      IconComponent: FiSettings,
+      label: t('share'),
+      action: () => () => navigate('/file-sharing/settings'),
+      icon: Share,
     },
     {
-      label: 'Logout',
-      action: () => {},
-      IconComponent: FiLogOut,
+      label: t('students'),
+      action: () => () => navigate('/'),
+      icon: Students,
     },
   ];
 
   return (
     <MenuBar
       menuItems={demoMenuItems}
-      title="MEINE DATEIEN"
-      logoImagePath="src/assets/icons/filesharing-light.svg"
+      title={t('fileSharing')}
+      icon={FileSharingIcon}
+      color="bg-ciDarkBlue"
     />
   );
 };
