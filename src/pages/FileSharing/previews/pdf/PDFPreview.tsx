93c5dd76474be95999065ff4753f961258ae1709
maddi02 <martin.hummel02@icloud.com>
2024-03-01 17:49:36 +0100

    Add PDF preview and file handling capabilities
    This commit implements PDF file preview and handling functionalities by integrating the library '@react-pdf/renderer'. Additionally, it introduces changes in 'PDFPreview.tsx' for managing PDF document rendering. Dependencies in 'package.json' and 'vite.config.ts' are updated respectively and static copy configurations are also introduced. This update enhances the application's ability to handle and display PDF files natively.

--- src/pages/FileSharing/previews/pdf/PDFPreview.tsx
+++ src/pages/FileSharing/previews/pdf/PDFPreview.tsx
@@ -1,15 +1,91 @@
-import React, { FC } from 'react';
-import Label from '@/components/ui/label';
-import { DirectoryFile } from '@/datatypes/filesystem';
+import React, { FC, useEffect, useState, useRef } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+import type { PDFDocumentProxy } from 'pdfjs-dist';
+import type { DirectoryFile } from '@/datatypes/filesystem';
+import { getFileNameFromPath } from '@/utils/common';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 interface PDFPreviewProps {
   file: DirectoryFile;
 }
 
-const PDFPreview: FC<PDFPreviewProps> = ({ file }) => (
-  <div>
-    <Label>PDF Preview for: {file.filename}</Label>
-  </div>
-);
+const PDFPreview: FC<PDFPreviewProps> = ({ file }) => {
+  const mediaUrl = `http://localhost:5173/webdav/${file.filename}`;
+  const [numPages, setNumPages] = useState<number | null>(null);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const [containerWidth, setContainerWidth] = useState<number>(400);
+
+  const onDocumentLoadSuccess = (document: PDFDocumentProxy) => {
+    setNumPages(document.numPages);
+  };
+
+  const goToPrevPage = () => setCurrentPage((current) => Math.max(current - 1, 1));
+  const goToNextPage = () => setCurrentPage((current) => Math.min(current + 1, numPages ?? current));
+
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        setContainerWidth(entry.contentRect.width);
+      });
+    });
+
+    if (containerRef.current) {
+      resizeObserver.observe(containerRef.current);
+    }
+
+    return () => {
+      if (containerRef.current) {
+        resizeObserver.unobserve(containerRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <div className="pdf-preview">
+      <header className="pb-4 pt-4">
+        <h1>PDF Preview: {getFileNameFromPath(file.filename)}</h1>
+      </header>
+      <div
+        className="pdf-container"
+        ref={containerRef}
+      >
+        <nav className="pdf-navigation">
+          <div className="flex  justify-between  rounded-lg bg-gray-200 p-2">
+            <button
+              onClick={goToPrevPage}
+              disabled={currentPage <= 1}
+              type="button"
+            >
+              Previous
+            </button>
+            <span>
+              Page {currentPage} of {numPages}
+            </span>
+            <button
+              onClick={goToNextPage}
+              disabled={currentPage >= (numPages ?? currentPage)}
+              type="button"
+            >
+              Next
+            </button>
+          </div>
+        </nav>
+        <Document
+          file={mediaUrl}
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          <Page
+            pageNumber={currentPage}
+            width={containerWidth}
+          />
+        </Document>
+      </div>
+    </div>
+  );
+};
 
 export default PDFPreview;
--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,51 @@
-import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { createRequire } from 'node:module';
+import { defineConfig, normalizePath } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
+const require = createRequire(import.meta.url);
+const cMapsDir = normalizePath(path.join(path.dirname(require.resolve('pdfjs-dist/package.json')), 'cmaps'));
+const standardFontsDir = normalizePath(
+  path.join(path.dirname(require.resolve('pdfjs-dist/package.json')), 'standard_fonts'),
+);
+
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-        },
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: cMapsDir, dest: '.' }, // Adjusted destination to the root of the output folder
+        { src: standardFontsDir, dest: '.' },
+      ],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    server: {
-        proxy: {
-            '/webdav': {
-                target: 'https://server.demo.multi.schule',
-                changeOrigin: true,
-                secure: false,
-                headers: {
-                    "Origin": "https://server.demo.multi.schule"
-                },
-            },
-            '/api': {
-                rewrite: path => path.replace(/^\/api/, ''),
-                target: 'https://server.demo.multi.schule:8001',
-                changeOrigin: true,
-                secure: false,
-                headers: {
-                    "Origin": "https://server.demo.multi.schule:8001"
-                },
-            },
+  },
+  server: {
+    proxy: {
+      '/webdav': {
+        target: 'https://server.demo.multi.schule',
+        changeOrigin: true,
+        secure: false,
+        headers: {
+          Origin: 'https://server.demo.multi.schule',
+        },
+      },
+      '/api': {
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        target: 'https://server.demo.multi.schule:8001',
+        changeOrigin: true,
+        secure: false,
+        headers: {
+          Origin: 'https://server.demo.multi.schule:8001',
+        },
+      },
 
-            //TODO docs
-        }
-
-    }
+      //TODO docs
+    },
+  },
 });
