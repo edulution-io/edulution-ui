1a9843a07ceec8d9666d9016acaccc34c146eed8
Michael Dold <michael.dold@outlook.de>
2024-03-07 08:23:58 +0100

    Adjust styling and dialog title

--- src/pages/Settings/SettingsPage.tsx
+++ src/pages/Settings/SettingsPage.tsx
@@ -168,9 +168,9 @@ const SettingsPage: React.FC = () => {
         modal
         open={mode === 'add'}
       >
-        <DialogContent className="data-[state=open]:animate-contentShow fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] text-black shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+        <DialogContent className="data-[state=open]:animate-contentShow fixed left-[50%] top-[40%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] text-black shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
           <DialogHeader>
-            <DialogTitle>{t('settings.title')}</DialogTitle>
+            <DialogTitle>{t('settings.addApp.title')}</DialogTitle>
             <DialogDescription>{t('settings.addApp.description')}</DialogDescription>
             <DialogClose asChild>
               <button
