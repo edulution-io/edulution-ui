9caa03e0fa43e89bfbc152ba09d36c30c7de33f8
Michael <michidold@users.noreply.github.com>
2024-03-15 15:56:27 +0100

    NIEDUUI-40 Implement NX (#11)
    * Installed nx and initialized
    
    * Initialized nx and moved src to apps/frontend
    
    * Fix start skript and tailwindconfig
    
    * Adjust scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nxViteTsPaths()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './apps/frontend/src'),
     },
   },
 });
