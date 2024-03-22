f31d1de3ac7ec068858d507a28b0502ee390b460
Michael Dold <michael.dold@outlook.de>
2024-03-22 16:00:16 +0100

    Add docker files

--- vite.config.ts
+++ vite.config.ts
@@ -14,11 +14,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/webdav': {
-        target: 'https://server.demo.multi.schule',
+        target: 'https://server.schulung.multi.schule',
         changeOrigin: true,
         secure: false,
         headers: {
-          Origin: 'https://server.demo.multi.schule',
+          Origin: 'https://server.schulung.multi.schule',
         },
       },
       '/api': {
