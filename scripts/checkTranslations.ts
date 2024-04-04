0750ea9132d88f35471aa45320bf67fa4db01700
Martin Hummel <90851855+Maddi02@users.noreply.github.com>
2024-04-04 22:55:28 +0200

    NIEDUUI-49: Implement script to verify translation files and update husky pre-com… (#18)
    * Implement script to verify translation files and update husky pre-commit hook
    
    A new script checking the keys consistency across different translation files has been implemented. As part of the pre-commit process, this script will now be run with the 'check-translations' command, ensuring all translation files are aligned before commit. Additionally, small changes have been made to the package.json dependencies.
    
    * Modify pre-commit hook in package.json
    
    The pre-commit hook script sequence in the package.json file has been updated. The refactored hook now includes an additional 'npm run lint' execution for linting the code right before the commit process. This change is made to ensure code consistency and to minimize potential issues or bugs.
    
    * Modify pre-commit hook in package.json
    
    The pre-commit hook script sequence in the package.json file has been updated. The refactored hook now includes an additional 'npm run lint' execution for linting the code right before the commit process. This change is made to ensure code consistency and to minimize potential issues or bugs.
    
    * Update translations and modify translation key check
    
    A new key-value pair has been added to the 'fileCreateNewContent' section of the English translation JSON file. Additionally, the translation key check script (checkTranslations.ts) has been modified to handle nested keys within the translation files. This improved key check process ensures consistency of keys across all layers of the translation files.

---
+++ scripts/checkTranslations.ts
@@ -0,0 +1,42 @@
+import * as fs from 'fs';
+
+const loadFile = (path: string): Record<string, unknown> =>
+  JSON.parse(fs.readFileSync(path, 'utf8')) as Record<string, unknown>;
+
+const deTranslations = loadFile('./apps/frontend/src/locales/de/translation.json');
+const enTranslations = loadFile('./apps/frontend/src/locales/en/translation.json');
+
+const flattenKeys = (obj: Record<string, unknown>): string[] => {
+  const keys: string[] = [];
+  const flatten = (object: Record<string, unknown>, path: string[] = []) => {
+    Object.keys(object).forEach((key) => {
+      if (typeof object[key] === 'object') {
+        flatten(object[key] as Record<string, unknown>, [...path, key]);
+      } else {
+        keys.push([...path, key].join('.'));
+      }
+    });
+  };
+  flatten(obj);
+  return keys;
+};
+
+const enNestedKeys = flattenKeys(enTranslations);
+const deNestedKeys = flattenKeys(deTranslations);
+
+const missingNestedInEN = deNestedKeys.filter((key) => !enNestedKeys.includes(key));
+const missingNestedInDE = enNestedKeys.filter((key) => !deNestedKeys.includes(key));
+
+if (missingNestedInEN.length > 0 || missingNestedInDE.length > 0) {
+  console.error('Translation files do not contain the same keys!');
+
+  if (missingNestedInEN.length > 0) {
+    console.error(`Missing nested keys in EN translation: ${missingNestedInEN.join(', ')}`);
+  }
+  if (missingNestedInDE.length > 0) {
+    console.error(`Missing nested keys in DE translation: ${missingNestedInDE.join(', ')}`);
+  }
+  process.exit(1);
+} else {
+  console.log('All keys and nested keys match.');
+}
