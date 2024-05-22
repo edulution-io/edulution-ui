87ecb341b87a26aa301e6cd8c68f24f309d40ba7
Michael Dold <michael.dold@outlook.de>
2024-05-22 11:12:45 +0200

    Removed unused

--- apps/frontend/src/pages/UserSettings/config.ts
+++ apps/frontend/src/pages/UserSettings/config.ts
@@ -1,25 +1,16 @@
 import { AntiMalwareIcon, UserIcon } from '@/assets/icons';
 import { MenuBarEntryProps } from '@/datatypes/types';
-import { Navigate } from 'react-router-dom';
 
 const USERSETTINGS_MENUBAR_CONFIG: MenuBarEntryProps = {
   title: 'usersettings.title',
   icon: UserIcon,
   color: 'hover:bg-ciLightBlue',
   menuItems: [
-    // {
-    //   id: 'general',
-    //   label: 'general',
-    //   icon: UserIcon,
-    //   action: () => {},
-    // },
     {
       id: 'security',
       label: 'usersettings.security',
       icon: AntiMalwareIcon,
-      action: () => {
-        Navigate({ to: '/user/security' });
-      },
+      action: () => {},
     },
   ],
 };
