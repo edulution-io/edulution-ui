25045f0fa85ecbd018f6fd356bd013ed7863e69c
Tom Lehmann <tom.lehmann@netzint.de>
2024-05-27 10:01:01 +0200

    Add native SOGo mail

--- apps/frontend/src/components/layout/BlankLayout.tsx
+++ apps/frontend/src/components/layout/BlankLayout.tsx
@@ -1,8 +1,6 @@
 import React, { PropsWithChildren } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import { useAuth } from 'react-oidc-context';
-
-import backgroundImage from '@/assets/background.jpg';
 import Header from '@/components/ui/Header';
 import Footer from '@/components/ui/Footer';
 import Sidebar from '../ui/Sidebar';
@@ -13,10 +11,7 @@ const BlankLayout: React.FC<PropsWithChildren> = () => {
   const isMainPage = pathname === '/';
 
   return (
-    <div
-      className="flex bg-cover bg-center opacity-90"
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
+    <div className="flex">
       <div className="flex min-h-[100vh] w-full flex-col px-5 lg:px-20">
         <Header isLogoShown={!isMainPage} />
         <main className="flex-1">
--- apps/frontend/src/components/layout/IframeLayout.tsx
+++ apps/frontend/src/components/layout/IframeLayout.tsx
@@ -1,23 +1,55 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
-
 import useAppConfigsStore from '@/store/appConfigsStore';
 import Sidebar from '@/components/ui/Sidebar';
-import backgroundImage from '@/assets/background.jpg';
 import { findAppConfigByName, getFromPathName } from '@/utils/common';
+import useUserStore from '@/store/userStore';
 
-const IframeLayout: React.FC = () => {
+interface IframeLayoutProps {
+  scriptOnStartUp?: string;
+  scriptOnStop?: string;
+}
+
+const IframeLayout: React.FC<IframeLayoutProps> = ({ scriptOnStartUp, scriptOnStop }) => {
   const { pathname } = useLocation();
   const rootPathName = getFromPathName(pathname, 1);
   const { appConfig } = useAppConfigsStore();
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+  const { isAuthenticated } = useUserStore();
+
+  const injectScript = (script: string) => {
+    const iframe = iframeRef.current;
+    if (iframe) {
+      iframe.onload = () => {
+        console.info('Injecting script into iframe.');
+        const iframeDocument = iframe.contentDocument || iframe.contentWindow?.document;
+        if (iframeDocument) {
+          const scriptElement = iframeDocument.createElement('script');
+          scriptElement.type = 'text/javascript';
+          scriptElement.innerHTML = script;
+          iframeDocument.head.appendChild(scriptElement);
+        }
+      };
+    }
+  };
+
+  useEffect(() => {
+    if (scriptOnStartUp && isAuthenticated) {
+      injectScript(scriptOnStartUp);
+    }
+  }, [isAuthenticated]);
+
+  useEffect(() => {
+    if (scriptOnStop && !isAuthenticated) {
+      injectScript(scriptOnStop);
+    }
+  }, [isAuthenticated]);
 
   return (
-    <div
-      className="flex bg-cover bg-center opacity-90"
-      style={{ backgroundImage: `url(${backgroundImage})` }}
-    >
+    <div className="flex">
       <div className="h-screen w-full">
         <iframe
+          ref={iframeRef}
           className="h-screen w-full pr-[58px]"
           title={pathname}
           src={findAppConfigByName(appConfig, rootPathName)?.options.url}
