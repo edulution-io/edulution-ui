8446369ff4f08a85cf751802a2432a2f88b584a3
Michael Dold <michael.dold@outlook.de>
2024-05-28 16:55:57 +0200

    Fix build issue

--- apps/frontend/src/pages/ConferencePage/useConferencesPageMenu.ts
+++ apps/frontend/src/pages/ConferencePage/useConferencesPageMenu.ts
@@ -6,7 +6,6 @@ const useConferencesPageMenu = () => {
     title: 'conferences.title',
     disabled: true,
     icon: ConferencesIcon,
-    disabled: true,
     color: 'hover:bg-ciDarkBlue',
     menuItems: [
       {
