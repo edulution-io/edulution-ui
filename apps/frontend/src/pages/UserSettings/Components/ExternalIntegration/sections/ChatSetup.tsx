577481a6df93ddd84e06770b648b86d82f07a775
Tom Lehmann <tom.lehmann@netzint.de>
2024-06-04 08:42:17 +0200

    Add external descriptions

--- apps/frontend/src/pages/UserSettings/Components/ExternalIntegration/sections/ChatSetup.tsx
+++ apps/frontend/src/pages/UserSettings/Components/ExternalIntegration/sections/ChatSetup.tsx
@@ -1,26 +1,140 @@
 import React from 'react';
+import i1 from './chat-images/windows-1.png';
+import i2 from './chat-images/windows-3.png';
+import i3 from './chat-images/windows-5.png';
+import a1 from './chat-images/android-1.jpg';
+import a2 from './chat-images/android-2.jpg';
+import a3 from './chat-images/android-3.jpg';
 
 const ChatSetup = () => {
   return (
     <div className="mb-4 rounded-lg p-6 shadow-lg">
-      <h2 className="mb-4 text-2xl font-semibold">Chat Setup</h2>
-      <ol className="list-inside list-decimal space-y-2">
-        <li className="text-lg">
-          <strong>Chat-Anwendung öffnen:</strong> Geben Sie die Domain in Ihren Webbrowser ein.
-        </li>
-        <li className="text-lg">
-          <strong>Anmelden:</strong> Verwenden Sie Ihren LDAP-Benutzernamen und Ihr Passwort.
-        </li>
-        <li className="text-lg">
-          <strong>Chat-Raum betreten:</strong> Wählen oder erstellen Sie einen Chat-Raum.
-        </li>
-        <li className="text-lg">
-          <strong>Nachrichten senden:</strong> Geben Sie Ihre Nachricht ein und senden Sie sie.
-        </li>
-        <li className="text-lg">
-          <strong>Benachrichtigungen:</strong> Achten Sie auf Hinweise über neue Nachrichten.
-        </li>
-      </ol>
+      <h2 className="mb-4 text-2xl font-semibold">Netzint Chat Matrix</h2>
+      <p>
+        Für die interne Kommunikation wird bei Netzint "Matrix" verwendet. Dabei gibt es folgende{' '}
+        <strong>Spaces</strong>:
+      </p>
+      <ul className="mb-4 list-inside list-disc">
+        <li>Netzint</li>
+        <li>Events</li>
+        <li>Arbeitskreise</li>
+        <li>Standortbezogene Spaces</li>
+      </ul>
+      <p>
+        Jeder Space ist untergliedert in verschiedene Räume zu speziellen Themen. Die meisten Räume sind für alle
+        Space-Mitglieder frei zugänglich, es können jedoch auch private Räume erstellt werden. Die frei zugänglichen
+        Räume im Space <strong>Netzint</strong> sind:
+      </p>
+      <ul className="mb-4 list-inside list-disc">
+        <li>Open: Allgemeine Infos, interessante News zu betrieblichen Themen</li>
+        <li>Important: Wichtige Infos, z.B. aktuelle Sicherheitslücken, wichtige Störungen</li>
+        <li>Kaffeeküche: Alles ist erlaubt :-)</li>
+        <li>Abwesenheit: Meldung von Abwesenheiten einzelner Personen, z.B. Krankheit, Urlaub</li>
+      </ul>
+      <p>
+        Generell ist es sinnvoll eine Antwort auf eine Nachricht als "Thread" abzuspalten. Somit ist der Lesefluss in
+        den einzelnen Räumen nicht gestört und andere Nutzer können auswählen, welche Themen bzw. Threads für sie
+        interessant sind und welche nicht.
+      </p>
+
+      <h3 className="mt-4 text-xl font-semibold">Installation</h3>
+      <p>
+        Es gibt einige Clients zur Auswahl, sie sollten alle untereinander funktionieren.{' '}
+        <a
+          href="https://matrix.org/ecosystem/clients/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 underline"
+        >
+          Hier
+        </a>{' '}
+        gibt es eine Übersicht. In dieser Dokumentation wird die App "Element" verwendet. Auch "Cinny" ist bereits
+        erfolgreich im Einsatz. Es ist möglich, sich auf mehreren Clients synchron einzuloggen (z.B. Smartphone und
+        Desktop), dazu muss man diese aber immer wieder mit einem bereits eingeloggten Client freigeben - aufgrund der
+        Verifizierung und Verschlüsselung. Der Key sollte an einem sicheren Ort abgespeichert werden (z.B.
+        Passwort-Manager).
+      </p>
+
+      <h4 className="mt-4 text-lg font-semibold">Desktop-Client</h4>
+      <p>
+        <a
+          href="https://packages.riot.im/desktop/install/win32/x64/Element%20Setup.exe"
+          className="text-blue-500 underline"
+        >
+          Desktop-Client
+        </a>{' '}
+        herunterladen & installieren.
+      </p>
+      <p>Auf Einloggen / Sign in klicken. Der Account existiert bereits via AD.</p>
+      <img
+        src={i1}
+        alt="Step 1"
+        className="my-4"
+      />
+      <p>Den Homeserver "matrix.org" ändern via Edit.</p>
+      <p>"matrix.netzint.de" als Homeserver eintragen und auf weiter / continue klicken.</p>
+      <img
+        src={i2}
+        alt="Step 3"
+        className="my-4"
+      />
+      <p>Einloggen mit AD-Login: vorname.nachname (ohne @netzint.de am Ende) + AD-Passwort.</p>
+      <p>Willkommen im lokalen Client.</p>
+      <img
+        src={i3}
+        alt="Step 5"
+        className="my-4"
+      />
+
+      <h4 className="mt-4 text-lg font-semibold">Mobile-Client</h4>
+      <p>
+        Smartphone-Client für{' '}
+        <a
+          href="https://play.google.com/store/apps/details?id=im.vector.app"
+          className="text-blue-500 underline"
+        >
+          Android
+        </a>{' '}
+        oder{' '}
+        <a
+          href="https://apps.apple.com/us/app/element-messenger/id1083446067"
+          className="text-blue-500 underline"
+        >
+          iPhone
+        </a>{' '}
+        herunterladen und installieren. "Ich habe bereits ein Konto" wählen, da der Account bereits existiert via AD.
+      </p>
+      <img
+        src={a1}
+        alt="Step 1"
+        className="my-4"
+      />
+      <p>"matrix.org" bearbeiten...</p>
+      <p>"matrix.netzint.de" eintragen und weiter klicken.</p>
+      <img
+        src={a2}
+        alt="Step 3"
+        className="my-4"
+      />
+      <p>Einloggen mit AD-Login: vorname.nachname (ohne @netzint.de am Ende) + AD-Passwort.</p>
+      <p>Willkommen im mobilen Client.</p>
+      <img
+        src={a3}
+        alt="Step 5"
+        className="my-4"
+      />
+
+      <h4 className="mt-4 text-lg font-semibold">Web-Client</h4>
+      <p>
+        Falls keine Client-Installation gewünscht ist, kann auch der{' '}
+        <a
+          href="https://chat.netzint.de"
+          className="text-blue-500 underline"
+        >
+          Element-Webclient
+        </a>{' '}
+        (selfhosted) verwendet werden. Auf Einloggen / Sign in klicken, der Account existiert bereits via AD.
+      </p>
     </div>
   );
 };
--- apps/frontend/src/pages/UserSettings/Components/ExternalIntegration/sections/MailSetup.tsx
+++ apps/frontend/src/pages/UserSettings/Components/ExternalIntegration/sections/MailSetup.tsx
@@ -1,8 +1,270 @@
 const MailSetup = () => {
   return (
     <div className="mb-4">
-      <h2 className="text-xl font-semibold">Mail Setup</h2>
       <p>Configure your mail settings here.</p>
+      <h2 className="mb-4 mt-4 text-2xl font-semibold">Netzint Maildocker Mailserver für Linuxmuster</h2>
+
+      <p className="p-4">Anhand der Demo Umgebung:</p>
+      <p className="p-4">
+        DNS Records setzen: (in der Annahme dass Webmail-Zugriff, IMAP, Active-Sync etc. auf der selben IP auflaufen wie
+        SMTP (in/out).
+      </p>
+      <pre>
+        <code>
+          {`# Name                            Type       Value
+@                                 MX 10      mailtransport.demo.mutli.schule
+mail.demo.mutli.schule            A          185.50.122.123
+mailtransport.demo.mutli.schule   A          185.50.122.123
+autodiscover.demo.mutli.schule    CNAME      mail.demo.mutli.schule
+admin-mail.demo.mutli.schule      CNAME      mail.demo.mutli.schule
+webmail.demo.mutli.schule         CNAME      mail.demo.mutli.schule
+autoconfig.demo.mutli.schule      CNAME      mail.demo.mutli.schule
+demo.multi.schule                 TXT        v=spf1 mx a -all
+_dmarc.demo.multi.schule          TXT        v=DMARC1; p=reject; fo=1;
+==> später in Anleitung: s1024._domainkey.demo.multi.schule <GENERIERTER-EINTRAG-AUS-Mailcow>`}
+        </code>
+      </pre>
+
+      <p className="p-4">Reverse DNS Records setzen:</p>
+      <pre>
+        <code>
+          {`# Name                            Type       Value
+123.122.50.185.in-addr.arpa       PTR        185.50.122.123`}
+        </code>
+      </pre>
+
+      <pre>
+        <code>
+          {`mkdir -p /srv/docker/
+cd /srv/docker
+git clone https://github.com/mailcow/mailcow-dockerized
+cd mailcow-dockerized/`}
+        </code>
+      </pre>
+
+      <p className="p-4">Konfig generieren:</p>
+      <pre>
+        <code>{`./generate_config.sh`}</code>
+      </pre>
+
+      <p className="p-4">
+        Bitte an der Firewall für die IP der Mailcow-VM eine Freigabe für folgende Dienste einrichten (sofern die DMZ
+        nur eingeschränkt ins Internet darf):
+      </p>
+      <ul className="list-inside list-disc p-4">
+        <li>ICMP ausgehend</li>
+        <li>DNS auf 1.1.1.1, 8.8.8.8 und 9.9.9.9 (oder eben DNS freischalten generell für diese VM)</li>
+        <li>NTP 123 ausgehend</li>
+      </ul>
+      <p className="p-4">Der Container wird beim Start div. Anfragen stellen, damit er intern sauber läuft.</p>
+
+      <p className="p-4">Container starten:</p>
+      <pre>
+        <code>{`docker compose up -d`}</code>
+      </pre>
+
+      <p className="p-4">
+        <a
+          href="https://$ipadresse"
+          className="text-blue-500 underline"
+        >
+          https://$ipadresse
+        </a>
+      </p>
+      <h4 className="p-4">Passwort ändern.</h4>
+
+      <p className="p-4">
+        API Key mit Read/write permissions in der Mailcow UI anlegen. Freigabe für die Docker Netzwerkebereiche:
+      </p>
+      <pre>
+        <code>
+          {`inet 172.17.0.1/16 brd 172.17.255.255 scope global docker0
+inet 172.22.1.1/24 brd 172.22.1.255 scope global br-mailcow`}
+        </code>
+      </pre>
+
+      <p className="p-4">
+        Binduser für Mailserver anlegen auf Schulserver. Wichtig! Dessen Passwort sollte nur alphanumerische Zeichen
+        beinhalten, am besten manuell nochmal setzen!
+      </p>
+      <p className="p-4">docker-compose.override.yml anlegen und befüllen</p>
+      <pre>
+        <code>
+          {`version: '2.1'
+services:
+    linuxmuster-mailcow:
+        image: netzint/linuxmuster-mailcow
+        container_name: mailcowcustomized_linuxmuster-mailcow
+        restart: always
+        volumes:
+            - ./data/conf/dovecot:/conf/dovecot:rw
+            - ./data/conf/sogo:/conf/sogo:rw
+        depends_on:
+            - nginx-mailcow
+            - dockerapi-mailcow
+            - php-fpm-mailcow
+            - sogo-mailcow
+            - dovecot-mailcow
+        environment:
+            - LINUXMUSTER_MAILCOW_LDAP_URI=ldaps://server.gam.multi.schule
+            - LINUXMUSTER_MAILCOW_LDAP_BASE_DN=OU=SCHOOLS,DC=gammertingen,DC=multi,DC=schule
+            - LINUXMUSTER_MAILCOW_LDAP_BIND_DN=cn=mail-binduser,OU=Management,OU=GLOBAL,DC=gammertingen,DC=multi,DC=schule
+            - LINUXMUSTER_MAILCOW_LDAP_BIND_DN_PASSWORD=23453425
+            - LINUXMUSTER_MAILCOW_API_KEY=46C9E7-49BD16---
+            - LINUXMUSTER_MAILCOW_SYNC_INTERVAL=300
+            - LINUXMUSTER_MAILCOW_DOMAIN_QUOTA=250000
+            - LINUXMUSTER_MAILCOW_ENABLE_GAL=1
+        networks:
+            mailcow-network:
+                aliases:
+                    - linuxmuster
+    sogo-mailcow:
+        volumes:
+            - ./data/conf/sogo/ldap.conf:/etc/ldap/ldap.conf:rw
+    dovecot-mailcow:
+        volumes:
+            - ./data/conf/sogo/ldap.conf:/etc/ldap/ldap.conf:rw`}
+        </code>
+      </pre>
+
+      <p className="p-4">
+        Falls der Mailserver den LDAPS Server direkt erreichen kann (zB. reverse Proxy von diesem im gleichen Netz), so
+        kann mittels Erweiterung der dovecot-mailcow und sogo-mailcow container die Kommunikation entsprechend
+        vereinfach werden. Beispiel:
+      </p>
+      <pre>
+        <code>
+          {`version: '2.1'
+services:
+    linuxmuster-mailcow:
+        image: netzint/linuxmuster-mailcow
+        container_name: mailcowcustomized_linuxmuster-mailcow
+        restart: always
+        volumes:
+            - ./data/conf/dovecot:/conf/dovecot:rw
+            - ./data/conf/sogo:/conf/sogo:rw
+        depends_on:
+            - nginx-mailcow
+            - dockerapi-mailcow
+            - php-fpm-mailcow
+            - sogo-mailcow
+            - dovecot-mailcow
+        environment:
+            - LINUXMUSTER_MAILCOW_LDAP_URI=ldap://10.0.0.1
+            - LINUXMUSTER_MAILCOW_LDAP_BASE_DN=OU=SCHOOLS,DC=linuxmuster,DC=lan
+            - LINUXMUSTER_MAILCOW_LDAP_BIND_DN=cn=mail-binduser,OU=Management,OU=GLOBAL,DC=linuxmuster,DC=lan
+            - LINUXMUSTER_MAILCOW_LDAP_BIND_DN_PASSWORD=vhLxVwy9YTcMg
+            - LINUXMUSTER_MAILCOW_API_KEY=E1CA71-3D8CE9-7D5928-72027C-164511
+            - LINUXMUSTER_MAILCOW_SYNC_INTERVAL=300
+            - LINUXMUSTER_MAILCOW_DOMAIN_QUOTA=250000
+            - LINUXMUSTER_MAILCOW_ENABLE_GAL=1
+        networks:
+            mailcow-network:
+                aliases:
+                    - linuxmuster
+        extra_hosts:
+          server.sgmaulbronn.de: 10.0.0.3
+
+    dovecot-mailcow:
+        volumes:
+            - ./data/conf/sogo/ldap.conf:/etc/ldap/ldap.conf:rw
+        extra_hosts:
+          server.sgmaulbronn.de: 10.0.0.3
+
+    sogo-mailcow:
+        volumes:
+            - ./data/conf/sogo/ldap.conf:/etc/ldap/ldap.conf:rw
+        extra_hosts:
+          server.sgmaulbronn.de: 10.0.0.3`}
+        </code>
+      </pre>
+
+      <p className="p-4">
+        <strong>WICHTIG:</strong> Als Base DN eine OU angeben, nicht direkt die TLD. Mit dieser fühlt es zu Fehlern und
+        die Dovecot Konfiguration funktioniert <strong>nicht</strong>.
+      </p>
+      <p className="p-4">
+        Ein Trusted Zertifikat ist nicht zwingend nötig solang LDAP verwendet wird. LDAPs funktioniert ausschließlich
+        mit passendem Zertifikat.
+      </p>
+      <p className="p-4">
+        Dem Sogo Mailcow fehlt ein Eintrag für die LDAP Zertifikate. Wir kopieren das einfach vom Host System:
+      </p>
+      <pre>
+        <code>{`cp /etc/ldap/ldap.conf ./data/conf/sogo/`}</code>
+      </pre>
+      <p className="p-4">Linuxmuster Mailcow container manuell anstarten und prüfen ob alles klappt:</p>
+      <pre>
+        <code>{`docker compose up`}</code>
+      </pre>
+      <p className="p-4">DKIM schlüssel für Domains anlegen in Mailcow UI (Konfiguration ARC/DKIM Keys)</p>
+
+      <h2 className="mb-4 mt-4 text-2xl font-semibold">Adapt SOGo Styling</h2>
+
+      <h4 className="pt-4">CSS</h4>
+      <p className="p-4">
+        (originally downloaded from:
+        <a
+          href="https://github.com/NlightN22/sogo-dark-red/tree/main"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 underline"
+        >
+          https://github.com/NlightN22/sogo-dark-red/tree/main
+        </a>
+        )
+      </p>
+      <p className="p-4">1. Adapt the 'custom-theme.css' file in the SOGo folder</p>
+
+      <h4 className="pt-4">Logo</h4>
+      <p className="p-4">Logo is displayed on the login page</p>
+      <p className="p-4">1. Replace the 'sogo-full.svg' file in the SOGo folder</p>
+
+      <h4 className="pt-4">Favicon</h4>
+      <p className="p-4">Icon for the tab in tab header</p>
+      <p className="p-4">
+        1. Replace the 'custom-favicon.ico' file in the SOGo folder (has to have format 32x32 64x64 128x128 256x256)
+      </p>
+
+      <h4 className="pt-4">Pagetitle</h4>
+      <p className="p-4">Name, Label for the tab in tab header</p>
+      <p className="p-4">1. Change the 'SOGoPageTitle' entry in the 'sogo.conf' file in the SOGo folder</p>
+
+      <h4 className="pt-4">Installing Open WebUI with Bundled Ollama Support</h4>
+      <p className="p-4">
+        This installation method uses a single container image that bundles Open WebUI with Ollama, allowing for a
+        streamlined setup via a single command. Choose the appropriate command based on your hardware setup:
+      </p>
+
+      <p className="p-4">With GPU Support: Utilize GPU resources by running the following command:</p>
+      <pre>
+        <code>
+          {`docker run -d -p 3000:8080 --gpus=all -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama`}
+        </code>
+      </pre>
+
+      <p className="p-4">For CPU Only: If you're not using a GPU, use this command instead:</p>
+      <pre>
+        <code>
+          {`docker run -d -p 3000:8080 -v ollama:/root/.ollama -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:ollama`}
+        </code>
+      </pre>
+
+      <p className="p-4">
+        Both commands facilitate a built-in, hassle-free installation of both Open WebUI and Ollama, ensuring that you
+        can get everything up and running swiftly.
+      </p>
+
+      <p className="p-4">
+        After installation, you can access Open WebUI at{' '}
+        <a
+          href="http://localhost:3000"
+          className="text-blue-500 underline"
+        >
+          http://localhost:3000
+        </a>
+        . Enjoy! 😄
+      </p>
     </div>
   );
 };
