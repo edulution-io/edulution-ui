904aa6761755e1dea2872845c25fdefb8bd84027
Michael Dold <michael.dold@outlook.de>
2024-06-11 09:34:39 +0200

    Consolidated config for vite, tailwind and prettier

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import path from 'path';
 
 export default defineConfig({
   test: {
@@ -19,11 +20,57 @@ export default defineConfig({
   },
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/frontend',
-
+  // resolve: {
+  //   alias: {
+  //     '@': path.resolve(__dirname, './apps/frontend/src'),
+  //     '@libs': path.resolve(__dirname, '../../libs'),
+  //   },
+  // },
   server: {
     port: 5173,
     host: 'localhost',
     fs: { strict: false },
+    proxy: {
+      '/auth': {
+        rewrite: (path) => path.replace(/^\/auth/, ''),
+        target: 'https://auth.demo.multi.schule/auth',
+        changeOrigin: true,
+        secure: false,
+        headers: {
+          Origin: 'https://ui.demo.multi.schule',
+          'X-Forwarded-For': 'client-ip-address',
+          'X-Forwarded-Proto': 'https',
+          'X-Forwarded-Host': 'auth.demo.multi.schule',
+        },
+      },
+      '/webdav': {
+        target: 'https://server.demo.multi.schule',
+        changeOrigin: true,
+        secure: false,
+        headers: {
+          Origin: 'https://server.demo.multi.schule',
+        },
+      },
+      '/api': {
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        target: 'https://server.demo.multi.schule:8001',
+        changeOrigin: true,
+        secure: false,
+        headers: {
+          Origin: 'https://server.demo.multi.schule:8001',
+        },
+      },
+      '/edu-api': {
+        rewrite: (path) => path.replace(/^\/edu-api/, ''),
+        target: 'http://localhost:3000/edu-api',
+        changeOrigin: false,
+        secure: false,
+        headers: {
+          Origin: 'https://ui.demo.multi.schule',
+        },
+      },
+      //TODO docs
+    },
   },
 
   preview: {
@@ -40,6 +87,7 @@ export default defineConfig({
 
   build: {
     outDir: '../../dist/apps/frontend',
+    emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
       transformMixedEsModules: true,
