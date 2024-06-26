3eebae2b03c7366be26510c19c61926ddb04cd31
Tom Lehmann <tom.lehmann@netzint.de>
2024-06-26 13:42:38 +0200

    Fix linuxmuster logout script

--- apps/frontend/src/pages/LinuxmusterPage/scripts/logout.ts
+++ apps/frontend/src/pages/LinuxmusterPage/scripts/logout.ts
@@ -1,5 +1,5 @@
 const logoutScript = `
-    const logoutButton = document.querySelector('a[ng\\\\:click="identity.logout()"]');
+    const logoutButton = document.querySelector('a[ng\\:click="identity.logout()"]');
     if (logoutButton) {
       logoutButton.click();
     }
