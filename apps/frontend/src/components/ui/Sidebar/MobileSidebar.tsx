aa6249afeccdddce5f5fb5251c0b2152cf41bbd9
Michael Dold <michael.dold@outlook.de>
2024-06-28 00:38:49 +0200

    SImplified code

--- apps/frontend/src/components/ui/Sidebar/MobileSidebar.tsx
+++ apps/frontend/src/components/ui/Sidebar/MobileSidebar.tsx
@@ -10,34 +10,30 @@ const MobileSidebar: React.FC<SidebarProps> = ({ sidebarItems }) => {
 
   useOnClickOutside(sidebarRef, isMobileSidebarOpen ? toggleMobileSidebar : () => {});
 
-  const renderMobileSidebar = () => (
-    <div
-      className="fixed right-0 top-0 z-[40] h-full w-full transform transition-transform duration-300 ease-in-out"
-      style={{ transform: `translateX(${isMobileSidebarOpen ? '0' : '100%'})` }}
-    >
-      <div
-        ref={sidebarRef}
-        className="fixed right-0 h-screen border-l-[1px] border-ciLightGrey bg-black bg-opacity-90 md:bg-none"
-      >
-        <div className="relative right-0 top-0 h-14 bg-black pr-4 pt-4" />
-        <div className="h-[calc(100%-56px)] overflow-auto">
-          <HomeButton />
-          {sidebarItems.map((item) => (
-            <MobileSidebarItem
-              key={item.link}
-              menuItem={item}
-            />
-          ))}
-          <UserMenuButton />
-        </div>
-      </div>
-    </div>
-  );
-
   return (
     <>
       <MobileMenuButton />
-      {renderMobileSidebar()}
+      <div
+        className="fixed right-0 top-0 z-[40] h-full w-full transform transition-transform duration-300 ease-in-out"
+        style={{ transform: `translateX(${isMobileSidebarOpen ? '0' : '100%'})` }}
+      >
+        <div
+          ref={sidebarRef}
+          className="fixed right-0 h-screen border-l-[1px] border-ciLightGrey bg-black bg-opacity-90 md:bg-none"
+        >
+          <div className="relative right-0 top-0 h-14 bg-black pr-4 pt-4" />
+          <div className="h-[calc(100%-56px)] overflow-auto">
+            <HomeButton />
+            {sidebarItems.map((item) => (
+              <MobileSidebarItem
+                key={item.link}
+                menuItem={item}
+              />
+            ))}
+            <UserMenuButton />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
