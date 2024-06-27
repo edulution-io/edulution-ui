f54de5cc7958f3f650b30b7226724eace76c4bbd
Michael Dold <michael.dold@outlook.de>
2024-06-27 16:16:55 +0200

    Fix styling and moved constants

--- apps/frontend/src/components/feature/Home/MobileFileAccess/MobileFileAccessSetupDialog.tsx
+++ apps/frontend/src/components/feature/Home/MobileFileAccess/MobileFileAccessSetupDialog.tsx
@@ -9,6 +9,7 @@ import { NavLink } from 'react-router-dom';
 import { IconContext } from 'react-icons';
 import useIsMobileView from '@/hooks/useIsMobileView';
 import { Card } from '@/components/shared/Card';
+import { EDU_APP_APPSTORE_URL } from '@libs/common/contants';
 
 type MobileFileAccessSetupDialogProps = {
   isOpen: boolean;
@@ -20,8 +21,6 @@ const MobileFileAccessSetupDialog: React.FC<MobileFileAccessSetupDialogProps> = 
   const { username } = useUserStore();
   const [isStepOne, setIsStepOne] = useState(true);
 
-  const urlAppStore = 'https://apps.apple.com/de/app/edulution-io/id6478116528';
-
   const qrCodeLoginData = {
     displayName: `${window.document.title}`,
     url: `${window.location.origin}/webdav`,
@@ -31,34 +30,48 @@ const MobileFileAccessSetupDialog: React.FC<MobileFileAccessSetupDialogProps> = 
   };
   const qrCodeLogin = JSON.stringify(qrCodeLoginData);
 
+  const iconContextValue = useMemo(() => ({ className: 'h-8 w-8 m-5' }), []);
+
   const loginDataTable = () => (
-    <div className="mt-4">
+    <div className="mt-4 overflow-auto">
       {t('form.url')}:
-      <Card
-        variant="text"
-        className=""
-      >
-        <pre className="m-2  text-foreground">{urlAppStore}</pre>
+      <Card variant="text">
+        <pre className="m-2 text-foreground">{qrCodeLoginData.url}</pre>
       </Card>
       {t('common.username')}:
-      <Card
-        variant="text"
-        className=""
-      >
+      <Card variant="text">
         <pre className="m-2 text-foreground">{username}</pre>
       </Card>
     </div>
   );
 
+  const navtoAppStoreButton = () => (
+    <div className="flex items-center justify-center">
+      <Button
+        variant="btn-outline"
+        className="flex items-center justify-center"
+      >
+        <NavLink
+          to={EDU_APP_APPSTORE_URL}
+          target="_blank"
+          className="flex flex-col items-center"
+        >
+          <IconContext.Provider value={iconContextValue}>
+            <MdOutlineFileDownload className="text-foreground" />
+          </IconContext.Provider>
+        </NavLink>
+      </Button>
+    </div>
+  );
+
   const getDialogBody = () => (
-    <div className="text-foreground">
+    <div className={`${isStepOne ? '' : 'min-w-[85%]'} text-foreground`}>
       <p className="flex justify-center">
         {t(isStepOne ? 'dashboard.mobileAccess.scanAppStoreLink' : 'dashboard.mobileAccess.scanAccessInfo')}
       </p>
       <div className="mt-4 justify-center">
-        <QRCodeDisplay value={isStepOne ? urlAppStore : qrCodeLogin} />
+        <QRCodeDisplay value={isStepOne ? EDU_APP_APPSTORE_URL : qrCodeLogin} />
       </div>
-
       <Button
         type="button"
         variant="btn-outline"
@@ -67,32 +80,24 @@ const MobileFileAccessSetupDialog: React.FC<MobileFileAccessSetupDialogProps> = 
       >
         {isStepOne ? <MdArrowForwardIos /> : <MdArrowBackIosNew />}
       </Button>
-
       <p className="mt-4">
         {t(isStepOne ? 'dashboard.mobileAccess.nextStepPreview' : 'dashboard.mobileAccess.copyCredentials')}
       </p>
-      {isStepOne ? null : loginDataTable()}
+      {isStepOne ? (
+        <>
+          <p className="my-4">{t('dashboard.mobileAccess.downloadDirect')}</p>
+          {navtoAppStoreButton()}
+        </>
+      ) : (
+        loginDataTable()
+      )}
     </div>
   );
 
-  const iconContextValue = useMemo(() => ({ className: 'h-8 w-8 m-5' }), []);
-
   const getSheetBody = () => (
     <div className="text-foreground">
-      <p className="my-4">{t('dashboard.mobileAccess.scanAppStoreLink')}</p>
-      <NavLink
-        to={urlAppStore}
-        className="flex flex-col items-center"
-      >
-        <Button
-          variant="btn-outline"
-          className="flex items-center justify-center"
-        >
-          <IconContext.Provider value={iconContextValue}>
-            <MdOutlineFileDownload className="text-foreground" />
-          </IconContext.Provider>
-        </Button>
-      </NavLink>
+      <p className="my-4">{t('dashboard.mobileAccess.downloadApp')}</p>
+      {navtoAppStoreButton()}
       <p className="mt-4">{t('dashboard.mobileAccess.accessData')}:</p>
       {loginDataTable()}
     </div>
