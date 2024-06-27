3b9b428fd8f952f8c020d29262f2b863872c5baf
Tom Lehmann <tom.lehmann@netzint.de>
2024-06-27 10:06:30 +0200

    Fix review findings

--- libs/src/error/CustomHttpException.ts
+++ libs/src/error/CustomHttpException.ts
@@ -2,15 +2,18 @@ import { HttpException, HttpStatus, Logger } from '@nestjs/common';
 import ErrorMessage from '@libs/error/errorMessage';
 
 class CustomHttpException extends HttpException {
-  constructor(errorMessage: ErrorMessage, status: HttpStatus, data?: unknown) {
+  constructor(errorMessage: ErrorMessage, status: HttpStatus, data?: unknown, domain?: string) {
     super(errorMessage, status);
-    CustomHttpException.logError(errorMessage, status, data);
+    CustomHttpException.logError(errorMessage, status, data, domain);
   }
 
-  private static logError(message: ErrorMessage, status: HttpStatus, data?: unknown) {
-    const domain = message.slice(0, message.indexOf('.'));
+  private static logError(message: ErrorMessage, status: HttpStatus, data?: unknown, domain?: string) {
+    const domainFallback = message.slice(0, message.indexOf('.'));
     const error = message.slice(message.lastIndexOf('.') + 1, message.length);
-    Logger.error(`HttpStatus: ${status}, Error: ${error}${data ? `, Data: ${JSON.stringify(data)}` : ''}`, domain);
+    Logger.error(
+      `HttpStatus: ${status}, Error: ${error}${data ? `, Data: ${JSON.stringify(data)}` : ''}`,
+      domain ?? domainFallback,
+    );
   }
 }
 
