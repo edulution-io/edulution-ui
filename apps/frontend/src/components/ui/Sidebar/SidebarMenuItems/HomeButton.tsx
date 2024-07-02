8ada54e4b0f5d9f42b72e0b852a7fac63cf119b1
Michael Dold <michael.dold@outlook.de>
2024-07-02 13:03:05 +0200

    Add homebutton toggle

--- apps/frontend/src/components/ui/Sidebar/SidebarMenuItems/HomeButton.tsx
+++ apps/frontend/src/components/ui/Sidebar/SidebarMenuItems/HomeButton.tsx
@@ -3,15 +3,18 @@ import { NavLink, useLocation } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { MobileLogoIcon } from '@/assets/icons';
 import { SIDEBAR_ICON_WIDTH } from '@libs/ui/constants';
+import useSidebarStore from '../sidebarStore';
 
 const HomeButton: React.FC = () => {
   const { t } = useTranslation();
   const { pathname } = useLocation();
+  const { toggleMobileSidebar } = useSidebarStore();
 
   return (
     <div key="home">
       <NavLink
         to="/"
+        onClick={toggleMobileSidebar}
         className={`group relative right-0 top-0 z-[99] flex cursor-pointer items-center justify-end gap-4 border-b-2 border-ciLightGrey bg-black px-4 py-2 hover:bg-black hover:opacity-90 md:block md:px-2 ${pathname === '/' ? 'bg-black' : ''}`}
       >
         <p className="text-md font-bold md:hidden">{t('home')}</p>
