756aa30e7a11797ac570aa61a44740745925c570
dominik.maier <dominik.maier@netzint.de>
2024-07-03 15:20:04 +0200

    NIEDUUI-208: * always use _id to filter surveys

--- apps/api/src/surveys/surveys.service.ts
+++ apps/api/src/surveys/surveys.service.ts
@@ -25,7 +25,7 @@ class SurveysService {
         HttpStatus.NOT_ACCEPTABLE,
       );
     }
-    const survey = this.surveyModel.findOne<Survey>({ id: surveyId }).exec();
+    const survey = this.surveyModel.findOne<Survey>({ _id: surveyId }).exec();
     if (survey == null) {
       throw new CustomHttpException(SurveyErrorMessages.NotAbleToFindSurveyError, HttpStatus.NOT_FOUND);
     }
@@ -33,7 +33,7 @@ class SurveysService {
   }
 
   async findSurveys(surveyIds: mongoose.Types.ObjectId[]): Promise<Survey[] | null> {
-    const surveys = this.surveyModel.find<Survey>({ id: { $in: surveyIds } }).exec();
+    const surveys = this.surveyModel.find<Survey>({ _id: { $in: surveyIds } }).exec();
     if (surveys == null) {
       throw new CustomHttpException(SurveyErrorMessages.NotAbleToFindSurveysError, HttpStatus.NOT_FOUND);
     }
@@ -42,7 +42,7 @@ class SurveysService {
 
   async deleteSurveys(surveyIds: mongoose.Types.ObjectId[]): Promise<void> {
     try {
-      await this.surveyModel.deleteMany({ id: { $in: surveyIds } }).exec();
+      await this.surveyModel.deleteMany({ _id: { $in: surveyIds } }).exec();
       Logger.log(`Deleted the surveys ${JSON.stringify(surveyIds)}`);
     } catch (error) {
       throw new CustomHttpException(SurveyErrorMessages.NotAbleToDeleteSurveyError, HttpStatus.NOT_MODIFIED, error);
@@ -82,7 +82,7 @@ class SurveysService {
       );
     }
 
-    const existingSurvey = await this.surveyModel.findOne<Survey>({ id: surveyId }).exec();
+    const existingSurvey = await this.surveyModel.findOne<Survey>({ _id: surveyId }).exec();
     if (!existingSurvey) {
       throw new CustomHttpException(SurveyErrorMessages.NotAbleToFindSurveyError, HttpStatus.NOT_FOUND);
     }
@@ -111,7 +111,7 @@ class SurveysService {
     answers.push(answer);
 
     const updatedSurvey = await this.surveyModel
-      .findOneAndUpdate<Survey>({ id: surveyId }, { publicAnswers: answers, participated })
+      .findOneAndUpdate<Survey>({ _id: surveyId }, { publicAnswers: answers, participated })
       .exec();
     if (updatedSurvey == null) {
       throw new CustomHttpException(SurveyErrorMessages.NotAbleToUpdateSurveyError, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -127,7 +127,7 @@ class SurveysService {
       );
     }
 
-    const survey = await this.surveyModel.findOne<Survey>({ id: surveyId }).exec();
+    const survey = await this.surveyModel.findOne<Survey>({ _id: surveyId }).exec();
     if (survey == null) {
       throw new CustomHttpException(SurveyErrorMessages.NotAbleToFindSurveyError, HttpStatus.NOT_FOUND);
     }
