6649982f064116afa2941d2efdd1dc56d3a3da26
Michael Dold <michael.dold@outlook.de>
2024-07-04 15:51:01 +0200

    Add custom error message

--- libs/src/error/errorMessage.ts
+++ libs/src/error/errorMessage.ts
@@ -1,6 +1,7 @@
+import AuthErrorMessages from '@libs/auth/authErrorMessages';
 import ConferencesErrorMessage from '@libs/conferences/types/conferencesErrorMessage';
 import GroupsErrorMessage from '@libs/user/types/groups/groupsErrorMessage';
 
-type ErrorMessage = ConferencesErrorMessage | GroupsErrorMessage;
+type ErrorMessage = ConferencesErrorMessage | GroupsErrorMessage | AuthErrorMessages;
 
 export default ErrorMessage;
