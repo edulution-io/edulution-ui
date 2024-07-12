f406bc557d63a7483cdc4c20393fcf245c2a491b
dominik.maier <dominik.maier@netzint.de>
2024-07-12 14:39:19 +0200

    deactivate imap functions for the preview

--- apps/api/src/mails/mails.service.ts
+++ apps/api/src/mails/mails.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { FetchMessageObject } from 'imapflow';
-import ImapFlowGetMailsClient from './imapFlowGetMailsClient';
+// import ImapFlowGetMailsClient from './imapFlowGetMailsClient';
+import mockedMails from "./dto/mocked-mails";
 
 @Injectable()
 class MailsService {
   constructor() {}
 
-  async getMails(username: string): Promise<FetchMessageObject[] > {
-    const ImapClient = new ImapFlowGetMailsClient(username);
-    await ImapClient.connect();
-    return await ImapClient.getMails();
+  async getMails(/*username: string*/): Promise<FetchMessageObject[] > {
+    // const ImapClient = new ImapFlowGetMailsClient(username);
+    // await ImapClient.connect();
+    // return await ImapClient.getMails();
+    return mockedMails;
   }
 }
 
