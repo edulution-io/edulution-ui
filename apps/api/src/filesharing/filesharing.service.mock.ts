070e3df066fe6ffafe0c7266a83f20a85f6d57f6
maddi02 <martin.hummel02@icloud.com>
2024-07-25 14:07:51 +0200

    removed the custom file

--- apps/api/src/filesharing/filesharing.service.mock.ts
+++ apps/api/src/filesharing/filesharing.service.mock.ts
@@ -1,24 +1,10 @@
 import { Readable } from 'stream';
 
-interface CustomFile {
-  buffer: Buffer;
-  originalName?: string;
-  fieldname: string;
-  originalname: string;
-  encoding: string;
-  mimetype: string;
-  size: number;
-  destination: string;
-  filename: string;
-  path: string;
-  stream: Readable;
-}
-
 const fileStream = new Readable();
 fileStream.push(Buffer.from('file content'));
 fileStream.push(null);
 
-const mockFile: CustomFile = {
+const mockFile = {
   buffer: Buffer.from('test content'),
   stream: fileStream,
   fieldname: 'uploadedFile',
