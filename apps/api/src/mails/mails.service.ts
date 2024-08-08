123a3e0146df395130d54c9d75ebdc8281241dff
dominik.maier <dominik.maier@netzint.de>
2024-08-08 17:27:54 +0200

    NIEDUUI-265: * uncomment the filter to only fetch for new mails

--- apps/api/src/mails/mails.service.ts
+++ apps/api/src/mails/mails.service.ts
@@ -51,8 +51,7 @@ class MailsService {
     const mails: MailDto[] = [];
     try {
       const fetchMail: AsyncGenerator<FetchMessageObject> = client.fetch(
-        // { or: [{ new: true }, { seen: false }, { recent: true }] },
-        '1:*',
+        { or: [{ new: true }, { seen: false }, { recent: true }] },
         {
           source: true,
           envelope: true,
