1b17349151fa43ec71c80ede6aa1251e4c0e1d7e
Tom Lehmann <tom.lehmann@netzint.de>
2024-08-08 13:57:56 +0200

    Remove unnecessary usermodule import

--- apps/api/src/lmnApi/lmnApi.module.ts
+++ apps/api/src/lmnApi/lmnApi.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import LmnApiService from './lmnApi.service';
 import { LmnApiController } from './lmnApi.controller';
-import UsersModule from '../users/users.module';
 
 @Module({
-  imports: [UsersModule],
   providers: [LmnApiService],
   controllers: [LmnApiController],
   exports: [LmnApiService],
