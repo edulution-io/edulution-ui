ac20136e2583700c7585f25a8dd8d1dbf837f221
maddi02 <martin.hummel02@icloud.com>
2024-08-16 14:17:25 +0200

    adjust FM Table for mobile

--- apps/frontend/src/components/layout/MainLayout.tsx
+++ apps/frontend/src/components/layout/MainLayout.tsx
@@ -5,18 +5,20 @@ import Footer from '@/components/ui/Footer';
 import { Sidebar } from '@/components';
 import MenuBar from '@/components/shared/MenuBar';
 import useMenuBarConfig from '@/hooks/useMenuBarConfig';
+import useIsMobileView from '@/hooks/useIsMobileView';
 
 const MainLayout: React.FC<PropsWithChildren> = () => {
   const { pathname } = useLocation();
   const menuBar = useMenuBarConfig();
   const isMainPage = pathname === '/';
+  const isMobile = useIsMobileView();
 
   return (
     <div className="flex">
       {!menuBar.disabled && !isMainPage && <MenuBar />}
       <div className={`flex h-[100vh] w-full flex-col ${!menuBar.disabled ? 'pl-2 pr-4 lg:px-20' : ''}`}>
         {!menuBar.disabled && isMainPage && <Header />}
-        <main className={`flex-1 ${!menuBar.disabled ? 'ml-[-4vh]' : ''}`}>
+        <main className={`flex-1 ${!menuBar.disabled && !isMobile ? 'ml-[-4vh]' : 'ml-4'}`}>
           {' '}
           <Outlet />
         </main>
