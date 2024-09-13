28e059ffe92c3e2aebc079ca2e0ba9c905981a10
Michael Dold <michael.dold@outlook.de>
2024-09-13 08:38:25 +0200

    Removed bearer auth for auth controller

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Post, Req } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
 import { SigninRequest } from 'oidc-client-ts';
 import { Request } from 'express';
 import AUTH_PATHS from '@libs/auth/constants/auth-endpoints';
@@ -7,7 +7,6 @@ import { Public } from '../common/decorators/public.decorator';
 import AuthService from './auth.service';
 
 @ApiTags(AUTH_PATHS.AUTH_ENDPOINT)
-@ApiBearerAuth()
 @Controller(AUTH_PATHS.AUTH_ENDPOINT)
 class AuthController {
   constructor(private readonly authService: AuthService) {}
