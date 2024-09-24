ac8042c6c7c67dc766c12bab5861241dcd8ac4c6
Michael Dold <michael.dold@outlook.de>
2024-09-24 13:19:11 +0200

    Add select member functionality

--- apps/frontend/src/pages/ClassManagement/LessonPage/LessonFloatingButtonsBar.tsx
+++ apps/frontend/src/pages/ClassManagement/LessonPage/LessonFloatingButtonsBar.tsx
@@ -59,7 +59,7 @@ const LessonFloatingButtonsBar: React.FC<FloatingButtonsBarProps> = ({ students 
       icon: FaArrowRightFromBracket,
       text: ClassMgmtFloatingButtons.Share,
       enableAction: async () => {
-        const shareDTO = buildShareDTO(user?.cn, member, moveOrCopyItemToPath);
+        const shareDTO = buildShareDTO(user?.cn, students, moveOrCopyItemToPath);
         if (!shareDTO) return;
         await shareFiles(shareDTO);
       },
@@ -69,7 +69,7 @@ const LessonFloatingButtonsBar: React.FC<FloatingButtonsBarProps> = ({ students 
       icon: FaArrowRightToBracket,
       text: ClassMgmtFloatingButtons.Collect,
       enableAction: async () => {
-        const collectDTO = buildCollectDTO(member, user, currentGroupName || '', user?.sophomorixIntrinsic2[0] || '');
+        const collectDTO = buildCollectDTO(students, user, currentGroupName || '', user?.sophomorixIntrinsic2[0] || '');
         if (!collectDTO) return;
         await collectFiles(collectDTO, user?.sophomorixRole || '');
       },
--- apps/frontend/src/pages/ClassManagement/LessonPage/UserArea/UserArea.tsx
+++ apps/frontend/src/pages/ClassManagement/LessonPage/UserArea/UserArea.tsx
@@ -8,6 +8,7 @@ import { SOPHOMORIX_STUDENT } from '@libs/lmnApi/constants/sophomorixRoles';
 import sortByName from '@libs/common/utils/sortByName';
 import useLmnApiStore from '@/store/useLmnApiStore';
 import useUserStore from '@/store/UserStore/UserStore';
+import Checkbox from '@/components/ui/Checkbox';
 
 const UserArea = () => {
   const { t } = useTranslation();
@@ -15,6 +16,7 @@ const UserArea = () => {
   const { user } = useUserStore();
   const { member } = useLessonStore();
   const [selectedMember, setSelectedMember] = useState<UserLmnInfo[]>([]);
+  const isMemberSelected = selectedMember.length;
 
   const isTeacherInSameClass = useMemo(
     () =>
@@ -33,35 +35,56 @@ const UserArea = () => {
     return teacher.sophomorixSchoolname === student.sophomorixSchoolname;
   };
 
+  const selectableMembers = member.filter((m) => m.sophomorixRole === SOPHOMORIX_STUDENT && isTeacherInSameSchool(m));
+
   const getSelectedStudents = () => {
-    if (selectedMember.length) {
+    if (isMemberSelected) {
       return selectedMember.filter((m) => m.sophomorixRole === SOPHOMORIX_STUDENT);
     }
-    return member.filter((m) => m.sophomorixRole === SOPHOMORIX_STUDENT && isTeacherInSameSchool(m));
+    return selectableMembers;
+  };
+
+  const areAllChecked = selectedMember.length === selectableMembers.length;
+
+  const onCheckAll = () => {
+    if (!areAllChecked) {
+      setSelectedMember(selectableMembers);
+    } else setSelectedMember([]);
   };
 
   return (
-    <div className="mt-3 h-full">
-      <h3 className="mb-2 text-center">
-        {member.length} {t('classmanagement.usersInThisSession')}
-        {selectedMember.length ? (
-          <div className="ml-4 text-lg md:inline">
-            ({selectedMember.length} {t('common.selected')})
-          </div>
-        ) : null}
-      </h3>
+    <div className="mt-3 h-full ">
+      <div className="flex items-center">
+        <div
+          className="flew-row ml-2 flex cursor-pointer"
+          onClickCapture={onCheckAll}
+        >
+          <Checkbox
+            className="color-white scale-125 rounded-lg"
+            checked={areAllChecked}
+            aria-label={t('select')}
+          />
+          <p className="ml-2">{t('selectAll')}</p>
+        </div>
+
+        <h3 className="mb-2 flex flex-grow justify-center text-center">
+          {member.length} {t('classmanagement.usersInThisSession')}{' '}
+          {isMemberSelected ? `(${isMemberSelected} ${t('common.selected')})` : null}
+        </h3>
+      </div>
       <div className="flex max-h-[calc(100vh-390px)] max-w-full flex-wrap overflow-y-auto overflow-x-visible scrollbar-thin md:max-h-[calc(100vh-240px)]">
         {member.sort(sortByName).map((m) => (
           <UserCard
             key={m.dn}
             user={m}
+            selectedMember={selectedMember}
             setSelectedMember={setSelectedMember}
             isTeacherInSameClass={isTeacherInSameClass(m)}
             isTeacherInSameSchool={isTeacherInSameSchool(m)}
           />
         ))}
       </div>
-      <LessonFloatingButtonsBar students={getSelectedStudents()} />
+      {isMemberSelected ? <LessonFloatingButtonsBar students={getSelectedStudents()} /> : null}
     </div>
   );
 };
