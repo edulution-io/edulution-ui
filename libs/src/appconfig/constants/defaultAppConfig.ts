b761cadfbe2769f10aeb355af3612e798008caf2
Michael Dold <michael.dold@outlook.de>
2024-10-09 15:42:17 +0200

    Add icons to library

--- libs/src/appconfig/constants/defaultAppConfig.ts
+++ libs/src/appconfig/constants/defaultAppConfig.ts
@@ -1,16 +1,39 @@
+import { FilesharingIcon, SurveysIcon, ClassManagementIcon, WhiteboardIcon } from '@libs/assets';
+import APPS from './apps';
+import APP_INTEGRATION_VARIANT from './appIntegrationVariants';
+
+const { FILE_SHARING, SURVEYS, CLASS_MANAGEMENT, WHITEBOARD } = APPS;
+const { NATIVE } = APP_INTEGRATION_VARIANT;
+
 const defaultAppConfig = [
   {
-    name: 'filesharing',
-    icon: '/src/assets/icons/edulution/Filesharing.svg',
-    appType: 'native',
+    name: FILE_SHARING,
+    icon: FilesharingIcon,
+    appType: NATIVE,
     options: {},
     accessGroups: [],
     extendedOptions: [],
   },
   {
-    name: 'survey',
-    icon: '/src/assets/icons/edulution/AnotherApp.svg',
-    appType: 'web',
+    name: SURVEYS,
+    icon: SurveysIcon,
+    appType: NATIVE,
+    options: {},
+    accessGroups: [],
+    extendedOptions: [],
+  },
+  {
+    name: CLASS_MANAGEMENT,
+    icon: ClassManagementIcon,
+    appType: NATIVE,
+    options: {},
+    accessGroups: [],
+    extendedOptions: [],
+  },
+  {
+    name: WHITEBOARD,
+    icon: WhiteboardIcon,
+    appType: NATIVE,
     options: {},
     accessGroups: [],
     extendedOptions: [],
