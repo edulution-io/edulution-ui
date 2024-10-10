78b6e5f698894b8a371b26358fdbd4859d635e20
Michael Dold <michael.dold@outlook.de>
2024-10-10 09:16:13 +0200

    Add new license dialog text

--- apps/frontend/src/pages/UserSettings/Info/CommunityLicenseDialog.tsx
+++ apps/frontend/src/pages/UserSettings/Info/CommunityLicenseDialog.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import useUserStore from '@/store/UserStore/UserStore';
 import AdaptiveDialog from '@/components/ui/AdaptiveDialog';
+import LinkText from '@/components/ui/LinkText';
 import { Button } from '@/components/shared/Button';
+import COMMUNITY_URL from '@libs/common/constants/communityLink';
 import useCommunityLicenseStore from './useCommunityLicenseStore';
 
 const CommunityLicenseDialog: React.FC = () => {
@@ -20,19 +22,39 @@ const CommunityLicenseDialog: React.FC = () => {
     return null;
   }
 
+  const urlObject = new URL(COMMUNITY_URL);
+  const rootUrl = `${urlObject.protocol}//${urlObject.hostname}`;
+
   const getDialogBody = () => (
-    <div className="flex flex-col items-center space-y-4">
-      <p className="mb-5">{t('licensing.communityLicenseDialog.description')}</p>
-      <Button
-        className="md:absolute md:bottom-4"
-        variant="btn-collaboration"
-        disabled={isLoading}
-        size="lg"
-        type="button"
-        onClick={() => close()}
-      >
-        {t('common.close')}
-      </Button>
+    <div className="flex flex-col items-start">
+      <p className="mb-5 text-left">
+        <Trans
+          i18nKey="licensing.communityLicenseDialog.description"
+          values={{ link: rootUrl }}
+          components={{
+            link1: (
+              <LinkText
+                to={COMMUNITY_URL}
+                title={rootUrl}
+              />
+            ),
+            strong: <strong />,
+            br: <br />,
+          }}
+        />
+      </p>
+      <div className="flex w-full justify-center">
+        <Button
+          className="md:absolute md:bottom-4"
+          variant="btn-collaboration"
+          disabled={isLoading}
+          size="lg"
+          type="button"
+          onClick={() => close()}
+        >
+          {t('common.close')}
+        </Button>
+      </div>
     </div>
   );
 
