c2225bf4f3fdaac0b7716ac40963115092705736
Tom Lehmann <tom.lehmann@netzint.de>
2024-10-15 15:10:50 +0200

    Remove loader class after rendering app

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,4 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+const root = document.getElementById('root');
+
+if (root) {
+  ReactDOM.createRoot(root).render(<App />);
+
+  window.addEventListener('load', () => {
+    root.classList.remove('loader');
+  });
+}
