8c9ff3d7105cb26e4521e0000e7b538222e3afcf
maierdominik <dominik.maier@netzing.de>
2024-10-21 17:05:46 +0200

    NIEDUUI-248: * revert the import of the surveys.modules to use MongooseModule.forFeature() and not the services

--- apps/api/src/surveys/surveys.module.ts
+++ apps/api/src/surveys/surveys.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import SurveySchema, { Survey } from './survey.schema';
+import SurveyAnswerSchema, { SurveyAnswer } from './survey-answer.schema';
 import SurveysService from './surveys.service';
 import SurveysController from './surveys.controller';
 import SurveyAnswersService from './survey-answer.service';
 import PublicSurveysController from './public-surveys.controller';
 
 @Module({
-  imports: [SurveysService, SurveyAnswersService],
+  imports: [
+    MongooseModule.forFeature([{ name: Survey.name, schema: SurveySchema }]),
+    MongooseModule.forFeature([{ name: SurveyAnswer.name, schema: SurveyAnswerSchema }]),
+  ],
   controllers: [SurveysController, PublicSurveysController],
   providers: [SurveysService, SurveyAnswersService],
 })
