eb5282b9cb1c5a687f23c42162a16775ceab72a2
maddi02 <martin.hummel02@icloud.com>
2024-11-06 20:23:37 +0100

    Added dynamic translations to the ScrollableTable.tsx component

--- apps/frontend/src/pages/UserSettings/Language/components/LanguageSelector.tsx
+++ apps/frontend/src/pages/UserSettings/Language/components/LanguageSelector.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useFormContext, useWatch } from 'react-hook-form';
 import RadioGroupFormField from '@/components/shared/RadioGroupFormField';
 import UserLanguage from '@libs/user/constants/userLanguage';
-import { NativeIcon, SettingsIcon } from '@/assets/icons';
+import { EnglishIcon, GermanIcon, SettingsIcon } from '@/assets/icons';
 import useUserStore from '@/store/UserStore/UserStore';
 import useIsMobileView from '@/hooks/useIsMobileView';
 import UserLanguageType from '@libs/user/types/userLanguageType';
@@ -17,13 +17,13 @@ const LanguageSelector: React.FC<SelectLanguageProps> = ({ settingLocation }) =>
       value: UserLanguage.GERMAN,
       translationId: `${settingLocation}.language.german`,
       disabled: false,
-      icon: NativeIcon,
+      icon: GermanIcon,
     },
     {
       value: UserLanguage.ENGLISH,
       translationId: `${settingLocation}.language.english`,
       disabled: false,
-      icon: NativeIcon,
+      icon: EnglishIcon,
     },
     {
       value: UserLanguage.SYSTEM_LANGUAGE,
