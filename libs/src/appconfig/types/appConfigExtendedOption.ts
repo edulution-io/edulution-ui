e85b34f50e87cadef922ff7af41b9e36c501d9f4
maddi02 <martin.hummel02@icloud.com>
2024-11-29 09:52:04 +0100

    Fix type definitions and add localization key.
    Updated the 'extendedOptions' initialization from an array to an object for consistency in 'AddAppConfigDialog.tsx'. Added 'editorialAccess' translation keys in both German and English translation files for improved localization. Corrected the generic type definition for 'AppConfigExtendedOption' and formatted code for better readability in 'appconfig' types.

--- libs/src/appconfig/types/appConfigExtendedOption.ts
+++ libs/src/appconfig/types/appConfigExtendedOption.ts
@@ -3,12 +3,12 @@ import { ColumnDef, SortingState } from '@tanstack/react-table';
 
 type TableConfig<TData> = {
   columns: ColumnDef<TData>[];
-  data: [];
+  data: TData[];
   sorting: SortingState;
   setSorting: (sorting: SortingState) => void;
 };
 
-export interface AppConfigExtendedOption<TData> {
+export interface AppConfigExtendedOption<TData = Record<string, unknown>> {
   name: string;
   title: string;
   description: string;
