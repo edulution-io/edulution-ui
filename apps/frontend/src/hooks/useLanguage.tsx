730a5fb786dcc4800cf002d0514d4fd2951d549e
maierdominik <dominik.maier@netzing.de>
2024-12-12 12:59:09 +0100

    299: * resolve conversations

--- apps/frontend/src/hooks/useLanguage.tsx
+++ apps/frontend/src/hooks/useLanguage.tsx
@@ -1,22 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useMemo } from 'react';
 import useUserStore from '@/store/UserStore/UserStore';
 
-const INITIAL_LANGUAGE = 'de';
-
 const useLanguage = () => {
-  const [language, setLanguage] = useState(INITIAL_LANGUAGE);
-
-  const { user } = useUserStore();
-
-  useEffect(() => {
-    if (user?.language && user?.language !== 'system') {
-      setLanguage(user?.language);
-      return;
+  const { language: userLanguage } = useUserStore().user || {};
+  const language = useMemo(() => {
+    if (userLanguage && userLanguage !== 'system') {
+      return userLanguage;
     }
     const currentLanguage = navigator?.language || 'de-DE';
-    const locale = currentLanguage.split('-')[0];
-    setLanguage(locale || INITIAL_LANGUAGE);
-  }, [user]);
+    return currentLanguage.split('-')[0];
+  }, [userLanguage]);
 
   return { language };
 };
