770d03318bbdfe7f7b9cdfbcc4933cf91ac9a941
maierdominik <dominik.maier@netzing.de>
2025-01-09 16:51:47 +0100

    342: * save changes

--- libs/src/appconfig/constants/extendedOptions/bulletinBoard.ts
+++ libs/src/appconfig/constants/extendedOptions/bulletinBoard.ts
@@ -1,7 +1,8 @@
 import ExtendedOptionKeys from '@libs/appconfig/constants/extendedOptionKeys';
 import ExtendedOptionField from '@libs/appconfig/constants/extendedOptionField';
+import { AppConfigExtendedOption } from '@libs/appconfig/types/appConfigExtendedOption';
 
-const BULLETIN_BOARD_EXTENDED_OPTIONS = [
+const BULLETIN_BOARD_EXTENDED_OPTIONS: AppConfigExtendedOption[] = [
   {
     name: ExtendedOptionKeys.BULLETIN_BOARD_CATEGORY_TABLE,
     description: 'appExtendedOptions.bulletinBoardUrl',
