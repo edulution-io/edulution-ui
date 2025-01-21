a27b8b5703a0ac8eb3749b62bb755d29d8aa4652
Tom Lehmann <tom.lehmann@netzint.de>
2025-01-21 16:28:27 +0100

    Add cookie access test, open conference in new tab

---
+++ libs/src/common/utils/testCookieAccess.ts
@@ -0,0 +1,51 @@
+import ExtendedCookieTestResult from '@libs/common/types/cookieTestResult';
+import COOKIE_TEST_URL from '@libs/common/constants/cookieTestUrl';
+
+async function testCookieAccess(): Promise<ExtendedCookieTestResult | null> {
+  try {
+    const response = await fetch(COOKIE_TEST_URL, { method: 'HEAD' });
+    if (!response.ok) {
+      return null;
+    }
+  } catch (err) {
+    return null;
+  }
+
+  return new Promise((resolve) => {
+    let resolved = false;
+
+    const iframe = document.createElement('iframe');
+    iframe.style.display = 'none';
+    iframe.src = COOKIE_TEST_URL;
+
+    function cleanupAndResolve(value: ExtendedCookieTestResult | null) {
+      if (!resolved) {
+        resolved = true;
+        // eslint-disable-next-line @typescript-eslint/no-use-before-define
+        window.removeEventListener('message', handleMessage);
+        if (iframe.parentNode) {
+          document.body.removeChild(iframe);
+        }
+        resolve(value);
+      }
+    }
+
+    function handleMessage(event: MessageEvent<ExtendedCookieTestResult>) {
+      const testUrlOrigin = new URL(COOKIE_TEST_URL).origin;
+      if (event.origin !== testUrlOrigin) return;
+
+      if (event.data.cookieTest === 'complete') {
+        cleanupAndResolve(event.data);
+      }
+    }
+
+    window.addEventListener('message', handleMessage);
+    document.body.appendChild(iframe);
+
+    iframe.onerror = () => {
+      cleanupAndResolve(null);
+    };
+  });
+}
+
+export default testCookieAccess;
