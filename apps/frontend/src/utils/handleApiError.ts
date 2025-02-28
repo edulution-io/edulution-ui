fec21eefe8ea2dd08e074cbaeec65a2c6f1c53ae
Tom Lehmann <tom.lehmann@netzint.de>
2025-02-28 12:31:53 +0100

    Remove eslint disable

--- apps/frontend/src/utils/handleApiError.ts
+++ apps/frontend/src/utils/handleApiError.ts
@@ -27,13 +27,12 @@ import { SHOW_TOASTER_DURATION } from '@libs/ui/constants/durations';
 const displayedErrors = new Set<string>();
 
 const handleApiError = (error: any, set: (params: any) => void, errorName = 'error') => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  if (error.name === 'CanceledError' || error.code === 'ERR_CANCELED') {
-    return;
-  }
-
   if (axios.isAxiosError(error)) {
     const axiosError = error as CustomAxiosError;
+
+    if (error.name === 'CanceledError' || error.code === 'ERR_CANCELED') {
+      return;
+    }
 
     const errorMessage = i18n.t(axiosError.response?.data?.message) || axiosError.response?.statusText;
 
