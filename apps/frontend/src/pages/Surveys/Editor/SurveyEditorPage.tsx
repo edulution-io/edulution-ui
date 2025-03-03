91603278b7fee338b8a770ebbaa042da0aa1f02e
Tom Lehmann <tom.lehmann@netzint.de>
2025-03-03 14:36:27 +0100

    Fix share survey dialog

--- apps/frontend/src/pages/Surveys/Editor/SurveyEditorPage.tsx
+++ apps/frontend/src/pages/Surveys/Editor/SurveyEditorPage.tsx
@@ -26,7 +26,6 @@ import { CREATED_SURVEYS_PAGE } from '@libs/survey/constants/surveys-endpoint';
 import getSurveyEditorFormSchema from '@libs/survey/types/editor/surveyEditorForm.schema';
 import SurveyEditor from '@/pages/Surveys/Editor/components/SurveyEditor';
 import SaveSurveyDialog from '@/pages/Surveys/Editor/dialog/SaveSurveyDialog';
-import SharePublicSurveyDialog from '@/pages/Surveys/Editor/dialog/SharePublicSurveyDialog';
 import SaveButton from '@/components/shared/FloatingsButtonsBar/CommonButtonConfigs/saveButton';
 import FloatingButtonsBarConfig from '@libs/ui/types/FloatingButtons/floatingButtonsBarConfig';
 import FloatingButtonsBar from '@/components/shared/FloatingsButtonsBar/FloatingButtonsBar';
@@ -146,7 +145,6 @@ const SurveyEditorPage = () => {
             submitSurvey={saveSurvey}
             isSubmitting={isLoading}
           />
-          <SharePublicSurveyDialog />
         </>
       )}
     </>
