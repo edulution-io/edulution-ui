c9bf68cd0fb8481cc0b7d09e225bc6ab237daca6
Tom Lehmann <tom.lehmann@netzint.de>
2025-03-06 12:02:26 +0100

    Add open in new tab to conference window

--- libs/src/dashboard/constants/pull-time-interval.ts
+++ libs/src/dashboard/constants/pull-time-interval.ts
@@ -10,9 +10,6 @@
  * You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-// pulls/fetches every 10 seconds
-const FEED_PULL_TIME_INTERVAL = 10000;
+const FEED_PULL_TIME_INTERVAL_SLOW = 60000;
 
-export default FEED_PULL_TIME_INTERVAL;
-
-export const FEED_PULL_TIME_INTERVAL_SLOW = 60000;
+export default FEED_PULL_TIME_INTERVAL_SLOW;
