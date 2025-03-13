ec5676db04ed21bde9b5c0d7f55efeeb825d42dc
maierdominik <dominik.maier@netzing.de>
2025-03-13 11:26:13 +0100

    558: * save automated fixes, changes

--- apps/frontend/src/components/ui/DateTimePicker/safeGetHours.ts
+++ apps/frontend/src/components/ui/DateTimePicker/safeGetHours.ts
@@ -10,8 +10,6 @@
  * You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-const safeGetHours = (dateValue: unknown): number => {
-  return dateValue instanceof Date ? dateValue.getHours() : 0;
-};
+const safeGetHours = (dateValue: unknown): number => (dateValue instanceof Date ? dateValue.getHours() : 0);
 
 export default safeGetHours;
