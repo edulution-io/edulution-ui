3a965be7b06807eed5b3be960ab7e4a9b662fae9
maierdominik <dominik.maier@netzing.de>
2025-05-05 14:03:54 +0200

    642: * enforce boolean type from isQuestionTypeChoiceType

--- libs/src/survey/utils/isQuestionTypeChoiceType.ts
+++ libs/src/survey/utils/isQuestionTypeChoiceType.ts
@@ -10,7 +10,7 @@
  * You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-const isQuestionTypeChoiceType = (questionType: string) =>
+const isQuestionTypeChoiceType = (questionType: string): boolean =>
   questionType === 'radiogroup' || questionType === 'checkbox' || questionType === 'dropdown';
 
 export default isQuestionTypeChoiceType;
