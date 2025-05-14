d6ba765f266f2bcdccc41eec7dbff3d577f285d8
Michael Dold <michael.dold@outlook.de>
2025-05-14 22:53:08 +0200

    Add health lib and endpoint

---
+++ apps/api/src/health/health.controller.ts
@@ -0,0 +1,53 @@
+/*
+ * LICENSE
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  MongooseHealthIndicator,
+  HttpHealthIndicator,
+  DiskHealthIndicator,
+} from '@nestjs/terminus';
+import EDU_API_CONFIG_ENDPOINTS from '@libs/appconfig/constants/appconfig-endpoints';
+import { Public } from '../common/decorators/public.decorator';
+
+const { EDUI_WEBDAV_URL, KEYCLOAK_API } = process.env;
+
+@Controller(EDU_API_CONFIG_ENDPOINTS.HEALTH_CHECK)
+class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private mongoose: MongooseHealthIndicator,
+    private httpIndicator: HttpHealthIndicator,
+    private disk: DiskHealthIndicator,
+  ) {}
+
+  @Public()
+  @Get()
+  @HealthCheck()
+  @HttpCode(HttpStatus.OK)
+  check() {}
+
+  @Get('readiness')
+  @HealthCheck()
+  readiness() {
+    return this.health.check([
+      () => this.mongoose.pingCheck('mongodb'),
+      () => this.httpIndicator.pingCheck('authServer', KEYCLOAK_API || ''),
+      () => this.httpIndicator.pingCheck('lmnServer', EDUI_WEBDAV_URL || ''),
+      () => this.disk.checkStorage('disk', { thresholdPercent: 0.8, path: '/' }),
+    ]);
+  }
+}
+
+export default HealthController;
--- apps/api/src/health/health.module.ts
+++ apps/api/src/health/health.module.ts
@@ -10,15 +10,12 @@
  * You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
-import EDU_API_CONFIG_ENDPOINTS from '@libs/appconfig/constants/appconfig-endpoints';
+import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
+import HealthController from './health.controller';
 
-@Controller(EDU_API_CONFIG_ENDPOINTS.HEALTH_CHECK)
-class HealthController {
-  @Get()
-  @HttpCode(HttpStatus.OK)
-  // eslint-disable-next-line @typescript-eslint/class-methods-use-this
-  check() {}
-}
-
-export default HealthController;
+@Module({
+  imports: [TerminusModule],
+  controllers: [HealthController],
+})
+export default class HealthModule {}
