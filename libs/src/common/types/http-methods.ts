ce9d1b14709c8f6cba47b007f7780b363ac96e6e
Tom Lehmann <tom.lehmann@netzint.de>
2025-05-14 14:28:04 +0200

    Fix api build

--- libs/src/common/types/http-methods.ts
+++ libs/src/common/types/http-methods.ts
@@ -53,7 +53,6 @@ export const HTTP_HEADERS = {
   Authorization: 'Authorization',
   XApiKey: 'x-api-key',
   CONNECTION_UID: 'Connection-Uid',
-  ContentLength: 'Content-Length',
   XForwaredFor: 'x-forwarded-for',
   UserAgent: 'User-Agent',
 } as const;
