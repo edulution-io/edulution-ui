39fc7e166dd1a364f14b0f353d3007313cf11747
martinhummel <martin.hummel02@icloud.com>
2025-05-27 16:46:47 +0200

    Fix: improve sorting function by directly accessing lastmod values

--- apps/frontend/src/pages/FileSharing/Table/FileSharingTableColumns.tsx
+++ apps/frontend/src/pages/FileSharing/Table/FileSharingTableColumns.tsx
@@ -147,8 +147,11 @@ const getFileSharingTableColumns = (
         return <span className="overflow-hidden text-ellipsis">{formattedDate}</span>;
       },
       sortingFn: (rowA, rowB, columnId) => {
-        const dateA = parseDate((rowA.original[columnId] as DirectoryFileDTO).lastmod);
-        const dateB = parseDate((rowB.original[columnId] as DirectoryFileDTO).lastmod);
+        const valueA = rowA.original.lastmod;
+        const valueB = rowB.original.lastmod;
+
+        const dateA = parseDate(valueA);
+        const dateB = parseDate(valueB);
 
         if (!dateA || !dateB) {
           return !dateA ? -1 : 1;
