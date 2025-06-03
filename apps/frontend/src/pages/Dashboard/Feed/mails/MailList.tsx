7a14efa37c46aaef2221424e7a44ea490b6cc0c1
maierdominik <dominik.maier@netzing.de>
2025-06-03 12:47:10 +0200

    889: * mails-feed remove the subject (should not be visible to students)

--- apps/frontend/src/pages/Dashboard/Feed/mails/MailList.tsx
+++ apps/frontend/src/pages/Dashboard/Feed/mails/MailList.tsx
@@ -48,7 +48,7 @@ const MailList = ({ items, className }: MailListProps) => {
         key={`${item.id}-BadgeSH-${label}`}
         variant={getBadgeVariantFromLabel(label)}
       >
-        {label}
+        {/* {label} */}
       </BadgeSH>
     ));
 
@@ -72,7 +72,6 @@ const MailList = ({ items, className }: MailListProps) => {
                 <p className="absolute h-2 w-2 rounded-full bg-ciLightGreen" />
               </div>
             </div>
-            <p className="break-all text-sm">{item.subject}</p>
             <p className="line-clamp-2 break-all text-xs text-muted-foreground">{item.text?.substring(0, 300)}</p>
             {renderLabelBadges(item)}
           </NavLink>
