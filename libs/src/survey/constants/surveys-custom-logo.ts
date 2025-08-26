9aa4612f25398b69e981552709cb549d8ccdc110
maierdominik <dominik.maier@netzing.de>
2025-08-26 15:52:32 +0200

    1079: * fix naming for the custom logo

--- libs/src/survey/constants/surveys-custom-logo.ts
+++ libs/src/survey/constants/surveys-custom-logo.ts
@@ -10,6 +10,6 @@
  * You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-const SURVEYS_CUSTOM_LOGO = 'surveys-default-logo.png';
+const SURVEYS_CUSTOM_LOGO = 'surveys-custom-logo.png';
 
 export default SURVEYS_CUSTOM_LOGO;
