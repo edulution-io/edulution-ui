64ad32af8870d153f06d54160ddd5f8983e4b025
martinhummel <martin.hummel02@icloud.com>
2025-09-16 16:18:11 +0200

    Refactor: simplify file selection logic in getFileSharingTableColumns

--- apps/frontend/src/pages/FileSharing/Table/getFileSharingTableColumns.tsx
+++ apps/frontend/src/pages/FileSharing/Table/getFileSharingTableColumns.tsx
@@ -87,8 +87,7 @@ const getFileSharingTableColumns = (
       accessorFn: (row) => row.type + row.filePath,
       cell: ({ row }) => {
         const [searchParams, setSearchParams] = useSearchParams();
-        const { currentlyDisabledFiles, setFileIsCurrentlyDisabled, setSelectedRows, selectedRows } =
-          useFileSharingStore();
+        const { currentlyDisabledFiles, setFileIsCurrentlyDisabled } = useFileSharingStore();
         const { resetCurrentlyEditingFile, setIsFilePreviewVisible, isFilePreviewDocked } = useFileEditorStore();
         const { setPublicDownloadLink } = useFileSharingDownloadStore();
         const isCurrentlyDisabled = currentlyDisabledFiles[row.original.filename];
@@ -107,18 +106,14 @@ const getFileSharingTableColumns = (
             setSearchParams(newParams);
             return;
           }
-          const rowId = row.original.filePath;
-          const toggleSelect = () => {
-            setSelectedRows({ ...selectedRows, [rowId]: !selectedRows[rowId] });
-          };
 
           if (!isValidFileToPreview(row.original) || isMobileView) {
-            toggleSelect();
+            row.toggleSelected();
             return;
           }
           const isOnlyOfficeDoc = isOnlyOfficeDocument(row.original.filename);
           if (isOnlyOfficeDoc && !isDocumentServerConfigured) {
-            toggleSelect();
+            row.toggleSelected();
             return;
           }
           if (isOnlyOfficeDoc) {
