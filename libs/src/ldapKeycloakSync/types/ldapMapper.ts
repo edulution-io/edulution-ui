9cc3432f5458c96c6a3bc35d4aca34a7ef154d50
Tom Lehmann <tom.lehmann@netzint.de>
2025-10-24 10:19:51 +0200

    Keycloak: Add scripts that check for missing mappers

--- libs/src/ldapKeycloakSync/types/ldapMapper.ts
+++ libs/src/ldapKeycloakSync/types/ldapMapper.ts
@@ -10,19 +10,13 @@
  * You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import removeRealmRoles from './removeRealmRoles';
-import addMailcowSyncRoles from './addMailcowSyncRoles';
-import patchEduUiClient from './patchEduUiClient';
-import addLdapGroupMappers from './addLdapGroupMappers';
-import addUserAttributeMappers from './addUserAttributeMappers';
+type LdapMapper = {
+  id: string;
+  name: string;
+  providerId: string;
+  providerType: string;
+  parentId: string;
+  config: Record<string, string[]>;
+};
 
-// Add new scripts here
-const keycloakConfigScripts = [
-  removeRealmRoles,
-  addMailcowSyncRoles,
-  patchEduUiClient,
-  addLdapGroupMappers,
-  addUserAttributeMappers,
-];
-
-export default keycloakConfigScripts;
+export default LdapMapper;
