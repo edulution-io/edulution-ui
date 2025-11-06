1db48279c5a59314d951af40fbb3f9009dda02b6
Michael <michidold@users.noreply.github.com>
2025-11-06 16:27:47 +0100

    Release 1.6.73 (#1484)
    * Fix wrong border
    
    * Bump version to 1.6.57 [skip ci]
    
    * Bump version to 1.6.58 [skip ci]
    
    * Removed history push
    
    * 1086: * to use our true translation function i18n we have to move the default survey values into the frontend
    
    * 1086: * remove the toString() on the default title, which did enforce the english translation
    
    * 470: * add the domain when throwing the CustomHttpException, if the bulletins attachment was not found
    
    * Bump version to 1.6.59 [skip ci]
    
    * 470: * add the domain to all the errors thrown in the bulletinboard.service
    
    * Refactor: remove unused `reset` function from `usePlatformStore` and clean `cleanAllStores` accordingly
    
    * Bump version to 1.6.60 [skip ci]
    
    * Bump version to 1.6.61 [skip ci]
    
    * Adjust nginx config, audit fix
    
    * Add header to locations
    
    * Bump version to 1.6.62 [skip ci]
    
    * Bump version to 1.6.63 [skip ci]
    
    * Fix review findings
    
    * Bump version to 1.6.64 [skip ci]
    
    * Fix review findings
    
    * Add force logout, fixed translations
    
    * AppConfigPage: Put URL field to the top
    
    * Add redirect on user change and on public pages
    
    * Fix translations
    
    * Fix bug if accounts are empty
    
    * Bump version to 1.6.65 [skip ci]
    
    * Bump version to 1.6.66 [skip ci]
    
    * MobileTopBar: Render also in tablet view
    
    * Show MobileTopBar always in edu app
    
    * Fix review findings
    
    * Fix review findings
    
    * Bump version to 1.6.67 [skip ci]
    
    * Bump version to 1.6.68 [skip ci]
    
    * Bump version to 1.6.69 [skip ci]
    
    * Fix review findings
    
    * Adjust script
    
    * Rework script
    
    * Changed license headers
    
    * Reset script
    
    * Bump version to 1.6.70 [skip ci]
    
    * Add debug log
    
    * Fix updating users in cache
    
    * Increase attempts, decrease concurrent requests
    
    * Add more logging, fix undefined
    
    * Bump version to 1.6.71 [skip ci]
    
    * Fix review findings
    
    * Change sync constants
    
    * Refactor loop
    
    * Refactor
    
    * Fix tests
    
    * Fix findings
    
    * Bump version to 1.6.72 [skip ci]
    
    * Bump version to 1.6.73 [skip ci]
    
    ---------
    
    Co-authored-by: martinhummel <martin.hummel02@icloud.com>
    Co-authored-by: Tom Lehmann <tom.lehmann@netzint.de>
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: maierdominik <dominik.maier@netzing.de>
    Co-authored-by: maierdominik <121227026+maierdominik@users.noreply.github.com>
    Co-authored-by: Martin Hummel <90851855+Maddi02@users.noreply.github.com>
    Co-authored-by: TomlDev <48552168+TomlDev@users.noreply.github.com>

--- apps/frontend/src/components/structure/layout/PageLayout.tsx
+++ apps/frontend/src/components/structure/layout/PageLayout.tsx
@@ -1,13 +1,20 @@
 /*
- * LICENSE
+ * Copyright (C) [2025] [Netzint GmbH]
+ * All rights reserved.
  *
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * This software is dual-licensed under the terms of:
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ * 1. The GNU Affero General Public License (AGPL-3.0-or-later), as published by the Free Software Foundation.
+ *    You may use, modify and distribute this software under the terms of the AGPL, provided that you comply with its conditions.
  *
- * You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *    A copy of the license can be found at: https://www.gnu.org/licenses/agpl-3.0.html
+ *
+ * OR
+ *
+ * 2. A commercial license agreement with Netzint GmbH. Licensees holding a valid commercial license from Netzint GmbH
+ *    may use this software in accordance with the terms contained in such written agreement, without the obligations imposed by the AGPL.
+ *
+ * If you are uncertain which license applies to your use case, please contact us at info@netzint.de for clarification.
  */
 
 import React from 'react';
@@ -18,6 +25,7 @@ import { useLocation } from 'react-router-dom';
 import FLOATING_BUTTONS_BAR_ID from '@libs/ui/constants/floatingButtonsBarId';
 import useUserAccounts from '@/hooks/useUserAccounts';
 import { getFromPathName } from '@libs/common/utils';
+import usePlatformStore from '@/store/EduApiStore/usePlatformStore';
 
 interface AppLayoutProps {
   nativeAppHeader?: NativeAppHeaderProps;
@@ -27,6 +35,7 @@ interface AppLayoutProps {
 
 const PageLayout = ({ nativeAppHeader, children, isFullScreen }: AppLayoutProps) => {
   const { pathname } = useLocation();
+  const isEdulutionApp = usePlatformStore((state) => state.isEdulutionApp);
   const rootPathName = getFromPathName(pathname, 1);
 
   useUserAccounts(rootPathName);
@@ -49,7 +58,7 @@ const PageLayout = ({ nativeAppHeader, children, isFullScreen }: AppLayoutProps)
 
       <div id={FLOATING_BUTTONS_BAR_ID} />
 
-      <Footer />
+      {!isEdulutionApp && <Footer />}
     </div>
   );
 };
