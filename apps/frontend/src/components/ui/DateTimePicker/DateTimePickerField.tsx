9ad920dd354bb2fb51e3e74d852066edd781f816
Tom Lehmann <tom.lehmann@netzint.de>
2025-11-26 14:28:13 +0100

    InfoBoard: Set enddate by default, change placeholder

--- apps/frontend/src/components/ui/DateTimePicker/DateTimePickerField.tsx
+++ apps/frontend/src/components/ui/DateTimePicker/DateTimePickerField.tsx
@@ -47,10 +47,11 @@ interface DateTimePickerFieldProps<T extends FieldValues> {
   variant?: DropdownVariant;
   allowPast?: boolean;
   isDateRequired?: boolean;
+  placeholder?: string;
 }
 
 const DateTimePickerField = <T extends FieldValues>(props: DateTimePickerFieldProps<T>) => {
-  const { form, path, translationId, variant = 'default', isDateRequired, allowPast } = props;
+  const { form, path, translationId, variant = 'default', isDateRequired, allowPast, placeholder } = props;
 
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
@@ -129,7 +130,7 @@ const DateTimePickerField = <T extends FieldValues>(props: DateTimePickerFieldPr
         minute: 'numeric',
         hour12: language === 'en',
       })
-    : t('form.input.dateTimePicker.placeholder');
+    : placeholder || t('form.input.dateTimePicker.placeholder');
 
   return (
     <Form {...form}>
