791ea94f10e0e57ac51812fc784cd6d318a11adc
martinhummel <martin.hummel02@icloud.com>
2026-02-26 12:00:28 +0100

    Refactor: convert function to arrow function in ResizableWindow; update icon in ToggleDockButton for consistency

--- apps/frontend/src/components/structure/framing/ResizableWindow/Buttons/ToggleDockButton.tsx
+++ apps/frontend/src/components/structure/framing/ResizableWindow/Buttons/ToggleDockButton.tsx
@@ -19,7 +19,7 @@
 
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRightToBracket, faWindowRestore } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRightToBracket, faUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 import WindowControlBaseButton from './WindowControlBaseButton';
 
 interface ToggleDockButtonProps {
@@ -32,7 +32,7 @@ const ToggleDockButton = ({ onClick, isDocked }: ToggleDockButtonProps) => (
     onClick={onClick}
     tooltipTranslationId={isDocked ? 'common.undock' : 'common.dock'}
   >
-    {isDocked ? <FontAwesomeIcon icon={faWindowRestore} /> : <FontAwesomeIcon icon={faArrowRightToBracket} />}
+    {isDocked ? <FontAwesomeIcon icon={faUpRightFromSquare} /> : <FontAwesomeIcon icon={faArrowRightToBracket} />}
   </WindowControlBaseButton>
 );
 
